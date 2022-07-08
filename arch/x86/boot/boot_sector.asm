5dc0c91d0a12550c5128260a3d327f239ac372fe
Moody Liu <mooodyhunter@outlook.com>
2022-07-09 00:42:13 +0100

    print_hex function

--- arch/x86/boot/boot_sector.asm
+++ arch/x86/boot/boot_sector.asm
@@ -1,15 +1,45 @@
+base:
     bits 16                 ; 16 bit code
-    [org 0x7C00]              ; where the code will be located
+    [org 0x7C00]            ; where the code will be located
 
 start:
-    mov si, welcome
+    mov si, welcome         ; welcome message
     call print_string
+
+    call print_newline
+
+    mov si, base_address    ; base address
+    call print_string
+    mov dx, base
+    call print_hex
+
+    call print_newline
+
+    mov si, strings_address ; strings address
+    call print_string
+    mov dx, strings
+    call print_hex
+
+    call print_newline
+
+    mov si, padding_address ; padding address
+    call print_string
+    mov dx, padding
+    call print_hex
+
+    call print_newline
+
     jmp $                   ; go and jump till the end of the world
 
 %include "utils.asm"
 
 strings:
-    welcome db "Welcome to the MOS!", 0x00
+    welcome         db "=========================", 0x0A, 0x0D
+                    db "|  Welcome to the MOS!  |", 0x0A, 0x0D
+                    db "=========================", 0x0A, 0x0D, 0x00
+    base_address    db "base:    ", 0x00
+    strings_address db "strings: ", 0x00
+    padding_address db "padding: ", 0x00
 
 ; padding up to the 512 - 2'th byte.
 ; we are filling the boot sector with zeros
--- arch/x86/boot/utils.asm
+++ arch/x86/boot/utils.asm
@@ -1,16 +1,70 @@
 ; print a single character from the register al, using the INT 10h call
+; **WARN: it's the caller's responsibility to preserve the previous value of AX**
 print_char:
     mov ah, 0x0e
     int 0x10                ; fire the interrupt
     ret
 
-; print a string from the memory location given by si
-; this is a recursive function
+print_newline:
+    mov ah, 0x0e
+    mov al, 0x0a
+    int 0x10                ; fire the interrupt
+    mov al, 0x0d
+    int 0x10                ; fire the interrupt
+    ret
+
+; print a null-terminated string from `SI`
+; **WARN: it's the caller's responsibility to make sure the string is null-terminated**
 print_string:
+    push ax
+    mov ah, 0x0e
+.loop:
     lodsb
     cmp al, 0x00            ; if the string is finished...
     je .done                ; return
-    call print_char
-    call print_string
+    int 0x10                ; print the character
+    jmp .loop
 .done:
+    pop ax
+    ret
+
+
+; prints a hexadecimal digit, given the number in `AL`
+print_single_hex_digit:
+    push ax
+    and al, 0x0f
+    cmp al, 0x09            ; if the number is greater than 9...
+    jg .print_hex_high      ; print the high digit
+.print_hex_lo:
+    add al, '0'             ; otherwise, print the low digit (0-9)
+    jmp .do_print
+.print_hex_high:
+    sub al, 0x0a
+    add al, 'A'             ; otherwise, print the high digit (A-F)
+.do_print:
+    call print_char
+    pop ax
+    ret
+
+
+; prints a number in the format `0x` in hexadecimal form, given the number in `DX`
+; **WARN: it's the caller's responsibility to make sure the number is in the correct range**
+print_hex:
+    push ax
+    push cx
+    mov al, '0'
+    call print_char
+    mov al, 'x'
+    call print_char
+    mov cl, 12
+.loop:
+    mov ax, dx
+    shr ax, cl
+    call print_single_hex_digit
+    sub cl, 4
+    cmp cl, 0
+    jns .loop
+.leave:
+    pop cx
+    pop ax
     ret
