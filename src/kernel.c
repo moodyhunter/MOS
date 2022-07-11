b07001c3a9dc990b813c0526a90c8e929edd3c76
Moody Liu <mooodyhunter@outlook.com>
2022-07-11 20:48:51 +0100

    fix ???? text

--- src/kernel.c
+++ src/kernel.c
@@ -4,7 +4,7 @@
 
 void start_kernel(void)
 {
-    const char *kernel_name = "Hello from the MffOS kedrnel ff";
+    const char *kernel_name = "Hello from the MOS Kernel!";
     print_string(kernel_name);
 
     float p = 0.0033314 + 0.0033314664;
