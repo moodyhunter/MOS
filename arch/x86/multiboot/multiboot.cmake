2e76df53184343bba9f91c26ed4e318ecb0caf9f
Moody Liu <mooodyhunter@outlook.com>
2022-07-11 18:41:07 +0100

    CMake cleanup

--- arch/x86/multiboot/multiboot.cmake
+++ arch/x86/multiboot/multiboot.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 add_nasm_binary(mos_multiboot_header SOURCE ${CMAKE_CURRENT_LIST_DIR}/multiboot.asm ELF_OBJECT)
-make_kernel_binary(
+add_kernel_binary(
     LOADER_ASSEMBLY_TARGET mos_multiboot_header::object
     LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/multiboot.ld)
--- arch/x86/raw_boot/raw_boot.cmake
+++ arch/x86/raw_boot/raw_boot.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 add_nasm_binary(mos_kernel_loader SOURCE ${CMAKE_CURRENT_LIST_DIR}/kernel_entry.asm ELF_OBJECT)
-make_kernel_binary(
+add_kernel_binary(
     LOADER_ASSEMBLY_TARGET mos_kernel_loader::object
     LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/raw_boot.ld)
 
