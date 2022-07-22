c4d51007b4ca5818402fb73e3c97a6d3f16c4111
Moody Liu <mooodyhunter@outlook.com>
2022-07-22 16:07:16 +0100

    install cross-compiler on CI

--- cmake/toolchains/cross-i686.cmake
+++ cmake/toolchains/cross-i686.cmake
@@ -1,7 +1,13 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 set(CMAKE_SYSTEM_PROCESSOR i686)
-set(CMAKE_C_COMPILER "i686-elf-gcc")
-set(CMAKE_LUNKER "i686-elf-ld")
 list(APPEND CMAKE_EXE_LINKER_FLAGS "-nostdlib -lgcc")
 list(APPEND CMAKE_C_FLAGS "-ffreestanding")
+
+find_program(CC_PATH NAMES "i686-linux-gnu-gcc" "i686-elf-gcc")
+if(CC_PATH)
+    message(STATUS "TOOLCHAIN: Found i686 gcc: ${CC_PATH}")
+    set(CMAKE_C_COMPILER ${CC_PATH})
+else()
+    message(FATAL_ERROR "TOOLCHAIN: Could not find i686 gcc")
+endif()
