f211458ab8bea9b2cab1e103000ef5f2b572e110
Moody Liu <mooodyhunter@outlook.com>
2022-07-25 22:15:41 +0100

    more printf tests

--- tests/printf/test.c
+++ tests/printf/test.c
@@ -85,6 +85,114 @@ TINY_TEST(integer)
     PRINTF_TEST("-0123", "%05d", -123);
     PRINTF_TEST("-00123", "%06d", -123);
     PRINTF_TEST("-000123", "%07d", -123);
+
+    // Precision
+    PRINTF_TEST("123", "%.0d", 123);
+    PRINTF_TEST("123", "%.1d", 123);
+    PRINTF_TEST("123", "%.2d", 123);
+    PRINTF_TEST("123", "%.3d", 123);
+    PRINTF_TEST("0123", "%.4d", 123);
+    PRINTF_TEST("00123", "%.5d", 123);
+    PRINTF_TEST("000123", "%.6d", 123);
+    PRINTF_TEST("0000123", "%.7d", 123);
+    PRINTF_TEST("00000123", "%.8d", 123);
+
+    // Precision with sign
+    PRINTF_TEST("+123", "%+.0d", 123);
+    PRINTF_TEST("+123", "%+.1d", 123);
+    PRINTF_TEST("+123", "%+.2d", 123);
+    PRINTF_TEST("+123", "%+.3d", 123);
+    PRINTF_TEST("+0123", "%+.4d", 123);
+    PRINTF_TEST("+00123", "%+.5d", 123);
+    PRINTF_TEST("+000123", "%+.6d", 123);
+    PRINTF_TEST("+0000123", "%+.7d", 123);
+    PRINTF_TEST("+00000123", "%+.8d", 123);
+
+    // Precision with zero padding
+    PRINTF_TEST("123", "%0.0d", 123);
+    PRINTF_TEST("123", "%0.1d", 123);
+    PRINTF_TEST("123", "%0.2d", 123);
+    PRINTF_TEST("123", "%0.3d", 123);
+    PRINTF_TEST("0123", "%0.4d", 123);
+    PRINTF_TEST("00123", "%0.5d", 123);
+    PRINTF_TEST("000123", "%0.6d", 123);
+    PRINTF_TEST("0000123", "%0.7d", 123);
+    PRINTF_TEST("00000123", "%0.8d", 123);
+
+    // Precision with zero padding and sign
+    // ! "If a precision is given with a numeric conversion (d, i, o, u, x, and X), the 0 flag is ignored."
+    PRINTF_TEST("+123", "%0+.0d", 123);
+    PRINTF_TEST("+123", "%0+.1d", 123);
+    PRINTF_TEST("+123", "%0+.2d", 123);
+    PRINTF_TEST("+123", "%0+.3d", 123);
+    PRINTF_TEST("+0123", "%0+.4d", 123);
+    PRINTF_TEST("+00123", "%0+.5d", 123);
+    PRINTF_TEST("+000123", "%0+.6d", 123);
+    PRINTF_TEST("+0000123", "%0+.7d", 123);
+    PRINTF_TEST("+00000123", "%0+.8d", 123);
+
+    // Precision with space (sign placeholder)
+    PRINTF_TEST(" 123", "% .0d", 123);
+    PRINTF_TEST(" 123", "% .1d", 123);
+    PRINTF_TEST(" 123", "% .2d", 123);
+    PRINTF_TEST(" 123", "% .3d", 123);
+    PRINTF_TEST(" 0123", "% .4d", 123);
+    PRINTF_TEST(" 00123", "% .5d", 123);
+    PRINTF_TEST(" 000123", "% .6d", 123);
+    PRINTF_TEST(" 0000123", "% .7d", 123);
+    PRINTF_TEST(" 00000123", "% .8d", 123);
+
+    // Precision with width
+
+    PRINTF_TEST("123", "%0.0d", 123);
+    PRINTF_TEST("123", "%0.1d", 123);
+    PRINTF_TEST("123", "%0.2d", 123);
+    PRINTF_TEST("123", "%0.3d", 123);
+    PRINTF_TEST("0123", "%0.4d", 123);
+    PRINTF_TEST("00123", "%0.5d", 123);
+    PRINTF_TEST("000123", "%0.6d", 123);
+    PRINTF_TEST("0000123", "%0.7d", 123);
+    PRINTF_TEST("00000123", "%0.8d", 123);
+
+    PRINTF_TEST("123", "%1.0d", 123);
+    PRINTF_TEST("123", "%1.1d", 123);
+    PRINTF_TEST("123", "%1.2d", 123);
+    PRINTF_TEST("123", "%1.3d", 123);
+    PRINTF_TEST("0123", "%1.4d", 123);
+    PRINTF_TEST("00123", "%1.5d", 123);
+    PRINTF_TEST("000123", "%1.6d", 123);
+    PRINTF_TEST("0000123", "%1.7d", 123);
+    PRINTF_TEST("00000123", "%1.8d", 123);
+
+    PRINTF_TEST("123", "%2.0d", 123);
+    PRINTF_TEST("123", "%2.1d", 123);
+    PRINTF_TEST("123", "%2.2d", 123);
+    PRINTF_TEST("123", "%2.3d", 123);
+    PRINTF_TEST("0123", "%2.4d", 123);
+    PRINTF_TEST("00123", "%2.5d", 123);
+    PRINTF_TEST("000123", "%2.6d", 123);
+    PRINTF_TEST("0000123", "%2.7d", 123);
+    PRINTF_TEST("00000123", "%2.8d", 123);
+
+    PRINTF_TEST("123", "%3.0d", 123);
+    PRINTF_TEST("123", "%3.1d", 123);
+    PRINTF_TEST("123", "%3.2d", 123);
+    PRINTF_TEST("123", "%3.3d", 123);
+    PRINTF_TEST("0123", "%3.4d", 123);
+    PRINTF_TEST("00123", "%3.5d", 123);
+    PRINTF_TEST("000123", "%3.6d", 123);
+    PRINTF_TEST("0000123", "%3.7d", 123);
+    PRINTF_TEST("00000123", "%3.8d", 123);
+
+    PRINTF_TEST(" 123", "%4.0d", 123);
+    PRINTF_TEST(" 123", "%4.1d", 123);
+    PRINTF_TEST(" 123", "%4.2d", 123);
+    PRINTF_TEST(" 123", "%4.3d", 123);
+    PRINTF_TEST("0123", "%4.4d", 123);
+    PRINTF_TEST("00123", "%4.5d", 123);
+    PRINTF_TEST("000123", "%4.6d", 123);
+    PRINTF_TEST("0000123", "%4.7d", 123);
+    PRINTF_TEST("00000123", "%4.8d", 123);
 }
 
 TINY_TEST_CASES test_cases[2] = {
