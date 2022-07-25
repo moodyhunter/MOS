102926b1cb17195a99528a2605ca44f714dc6f89
Moody Liu <mooodyhunter@outlook.com>
2022-07-25 20:04:37 +0100

    run tests in the CI environment

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,12 +3,15 @@
 # get the current directory
 DIR=$(pwd)
 
-qemu-system-i386 -kernel $DIR/mos_multiboot.bin -monitor "unix:/tmp/monitor.sock,server,nowait" &
+qemu-system-i386 -kernel $DIR/mos_multiboot.bin -monitor "unix:/tmp/monitor.sock,server,nowait" -nographic &
+pid=$!
 sleep 5
 
-if pgrep -x "qemu-system-i386" >/dev/null; then
-    echo "qemu-system-i386 is still running, probably the test has failed"
-    printf '%s\n' "screendump failure.ppm" "q" | nc -U /tmp/monitor.sock
-else
-    echo "Test passed."
-fi
+ps -p $pid >/dev/null
+
+[ $? == 1 ] && echo "Test passed." && exit 0
+
+echo "qemu-system-i386 is still running, probably the test has failed."
+printf '%s\n' "screendump failure.ppm" "q" | nc -U /tmp/monitor.sock
+echo "Test failed, see failure.ppm for the screen dump."
+exit 1
