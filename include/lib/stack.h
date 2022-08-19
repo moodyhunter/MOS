582f2524ec9a10c5fc5601896cc597e7e8da7481
Moody Liu <mooodyhunter@outlook.com>
2022-08-20 00:43:06 +0100

    lib: add a downwards-growing stack

---
+++ include/lib/stack.h
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include "mos/types.h"
+
+// A stack that grows downwards
+
+typedef struct
+{
+    void *const base;
+    void *head;
+    size_t capacity;
+} downwards_stack_t;
+
+void stack_init(downwards_stack_t *stack, void *mem_region_bottom, size_t size);
+void stack_deinit(downwards_stack_t *stack);
+
+void stack_push(downwards_stack_t *stack, void *data, size_t size);
+
+// ! WARN: Caller must ensure the data is at least size bytes long
+void stack_pop(downwards_stack_t *stack, void *data, size_t size);
---
+++ lib/stack.c
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "lib/stack.h"
+
+#include "lib/string.h"
+#include "mos/mm/kmalloc.h"
+#include "mos/printk.h"
+
+void stack_init(downwards_stack_t *stack, void *mem_region_bottom, size_t size)
+{
+    // the stack grows downwards, so the base of the stack is the bottom of the memory region
+    void *stack_base = (void *) ((uintptr_t) mem_region_bottom + size);
+
+    *((void **) &stack->base) = stack_base;
+    stack->head = stack_base;
+    stack->capacity = size;
+}
+
+void stack_deinit(downwards_stack_t *stack)
+{
+    memset(stack, 0, sizeof(downwards_stack_t));
+}
+
+void stack_push(downwards_stack_t *stack, void *data, size_t size)
+{
+    // high memory | base -----> head -----> base - capacity | low memory
+    if (unlikely((uintptr_t) stack->head - ((uintptr_t) stack->base - stack->capacity) < size))
+    {
+        mos_warn("stack overflow on stack %p, attempted to push %zu bytes", (void *) stack, size);
+        return;
+    }
+
+    stack->head = (void *) ((uintptr_t) stack->head - size);
+    memcpy(stack->head, data, size);
+}
+
+void stack_pop(downwards_stack_t *stack, void *data, size_t size)
+{
+    // high memory | base -----> head -----> base - capacity | low memory
+    if (unlikely((uintptr_t) stack->head - (uintptr_t) stack->base < size))
+    {
+        mos_warn("stack underflow on stack %p, attempted to pop %zu bytes", (void *) stack, size);
+        return;
+    }
+
+    memcpy(data, stack->head, size);
+    // memset(stack->head, 0, size);
+    stack->head = (void *) ((uintptr_t) stack->head + size);
+}
