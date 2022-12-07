69f8ce3dd36e197c79c803cae04018b8dbbb817c
Moody Liu <mooodyhunter@outlook.com>
2022-12-07 15:10:54 +0000

    process: don't allocate heap spaces initially

--- kernel/tasks/process.c
+++ kernel/tasks/process.c
@@ -105,7 +105,7 @@ process_t *process_new(process_t *parent, uid_t euid, const char *name, terminal
 
     thread_new(proc, THREAD_FLAG_USERMODE, entry, arg);
 
-    vmblock_t heap = platform_mm_alloc_pages(proc->pagetable, 1, PGALLOC_HINT_UHEAP, VM_READ | VM_WRITE | VM_USER);
+    vmblock_t heap = platform_mm_alloc_pages(proc->pagetable, 0, PGALLOC_HINT_UHEAP, VM_READ | VM_WRITE | VM_USER);
     process_attach_mmap(proc, heap, VMTYPE_HEAP, MMAP_DEFAULT);
 
     void *old_proc = hashmap_put(process_table, &proc->pid, proc);
