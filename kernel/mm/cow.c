9526034436bf66e0435cd681f4fe10efd2280f13
Moody Liu <mooodyhunter@outlook.com>
2022-12-07 23:14:41 +0000

    mm: comment on 'mm_make_process_map_cow' that the return value isn't trustworthy

--- kernel/mm/cow.c
+++ kernel/mm/cow.c
@@ -13,6 +13,8 @@
 
 vmblock_t mm_make_process_map_cow(paging_handle_t from, uintptr_t fvaddr, paging_handle_t to, uintptr_t tvaddr, size_t npages)
 {
+    // Note that the block returned by this function contains it's ACTRUAL flags, which may be different from the flags of the original block.
+    // (consider the case where we CoW-map a page that is already CoW-mapped)
     vmblock_t block = platform_mm_copy_maps(from, fvaddr, to, tvaddr, npages);
     platform_mm_flag_pages(from, fvaddr, npages, block.flags & ~VM_WRITE);
     platform_mm_flag_pages(to, tvaddr, npages, block.flags & ~VM_WRITE);
