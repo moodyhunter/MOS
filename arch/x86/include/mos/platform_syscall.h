fc8cbd9aa03ab5403e5372ea11efe218f7562512
Moody Liu <mooodyhunter@outlook.com>
2022-12-07 23:16:45 +0000

    x86 syscall: don't 'always_inline'

--- arch/x86/include/mos/platform_syscall.h
+++ arch/x86/include/mos/platform_syscall.h
@@ -5,42 +5,42 @@
 
 // TODO: rewrite in ASM
 
-always_inline long platform_syscall0(long number)
+should_inline long platform_syscall0(long number)
 {
     long result = 0;
     __asm__ volatile("int $0x88" : "=a"(result) : "a"(number));
     return result;
 }
 
-always_inline long platform_syscall1(long number, long arg1)
+should_inline long platform_syscall1(long number, long arg1)
 {
     long result = 0;
     __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1));
     return result;
 }
 
-always_inline long platform_syscall2(long number, long arg1, long arg2)
+should_inline long platform_syscall2(long number, long arg1, long arg2)
 {
     long result = 0;
     __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2));
     return result;
 }
 
-always_inline long platform_syscall3(long number, long arg1, long arg2, long arg3)
+should_inline long platform_syscall3(long number, long arg1, long arg2, long arg3)
 {
     long result = 0;
     __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3));
     return result;
 }
 
-always_inline long platform_syscall4(long number, long arg1, long arg2, long arg3, long arg4)
+should_inline long platform_syscall4(long number, long arg1, long arg2, long arg3, long arg4)
 {
     long result = 0;
     __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3), "S"(arg4));
     return result;
 }
 
-always_inline long platform_syscall5(long number, long arg1, long arg2, long arg3, long arg4, long arg5)
+should_inline long platform_syscall5(long number, long arg1, long arg2, long arg3, long arg4, long arg5)
 {
     long result = 0;
     __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3), "S"(arg4), "D"(arg5));
