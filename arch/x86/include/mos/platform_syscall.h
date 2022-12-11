f5d97ae8126b436cb27885fac2ad27c0d4c9d8a3
Moody Liu <mooodyhunter@outlook.com>
2022-12-11 14:49:28 +0000

    syscall: implement platform_syscall6

--- arch/x86/include/mos/platform_syscall.h
+++ arch/x86/include/mos/platform_syscall.h
@@ -46,3 +46,21 @@ should_inline long platform_syscall5(long number, long arg1, long arg2, long arg
     __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3), "S"(arg4), "D"(arg5));
     return result;
 }
+
+should_inline long platform_syscall6(long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6)
+{
+    long result = 0;
+    __asm__ volatile("push %%ebp\n"
+                     "mov %1, %%eax\n"
+                     "mov %2, %%ebx\n"
+                     "mov %3, %%ecx\n"
+                     "mov %4, %%edx\n"
+                     "mov %5, %%esi\n"
+                     "mov %6, %%edi\n"
+                     "mov %7, %%ebp\n"
+                     "int $0x88\n"
+                     "pop %%ebp\n"
+                     : "=a"(result)
+                     : "m"(number), "m"(arg1), "m"(arg2), "m"(arg3), "m"(arg4), "m"(arg5), "m"(arg6));
+    return result;
+}
