432557d34211f136171919355d5eaf03ee571493
Moody Liu <mooodyhunter@outlook.com>
2022-12-19 02:40:52 +0000

    doc: add mm group

--- kernel/include/mos/mm/mm_types.h
+++ kernel/include/mos/mm/mm_types.h
@@ -5,8 +5,16 @@
 #include "mos/platform/platform.h"
 #include "mos/types.h"
 
+/**
+ * @defgroup mm Memory Management
+ * @brief Memory management subsystem
+ * @{
+ */
+
 typedef struct
 {
     paging_handle_t address_space;
     vmblock_t block;
 } shm_block_t;
+
+/** @} */
