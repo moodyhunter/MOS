bf65a17ca16557ddb7c91d32c9fd817400e4ab71
Moody Liu <mooodyhunter@outlook.com>
2023-01-29 02:34:47 +0000

    ipc: add docs

--- kernel/include/mos/ipc/ipc.h
+++ kernel/include/mos/ipc/ipc.h
@@ -7,8 +7,29 @@
 
 void ipc_init(void);
 
+/*
+ * @brief Create a new IPC server
+ * @param name The name of the server
+ * @param max_pending_connections The maximum number of pending connections to allow
+ * @return A new io_t object that represents the server, or NULL on failure
+ *
+ * @note The io_t returned by this function is only to accept new connections or close the server, reading or writing to it will fail.
+ */
 io_t *ipc_create(const char *name, size_t max_pending_connections);
 
+/*
+ * @brief Accept a new connection on an IPC server
+ * @param server The server to accept a connection on
+ * @return A new io_t object that represents the connection to the client, or NULL on failure
+ */
 io_t *ipc_accept(io_t *server);
 
+/*
+ * @brief Connect to an IPC servers
+ * @param owner The process that owns the connection
+ * @param name The name of the server to connect to
+ * @param flags Flags to control the connection
+ * @param buffer_size The size of a shared-memory buffer to use for the connection
+ * @return A new io_t object that represents the connection, or NULL on failure
+ */
 io_t *ipc_connect(process_t *owner, const char *name, ipc_connect_flags flags, size_t buffer_size);
