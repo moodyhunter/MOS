75acf043115121f1f9f805cc2e883e8bf957d06c
Moody Liu <mooodyhunter@outlook.com>
2023-01-29 00:56:23 +0000

    x86 ports: use 'should_inline' instead of 'always_inline'

--- arch/x86/include/mos/x86/devices/port.h
+++ arch/x86/include/mos/x86/devices/port.h
@@ -7,38 +7,38 @@
 
 typedef u16 x86_port_t;
 
-always_inline u8 port_inb(u16 port)
+should_inline u8 port_inb(u16 port)
 {
     u8 value;
     __asm__ volatile("inb %1, %0" : "=a"(value) : "dN"(port));
     return value;
 }
 
-always_inline u16 port_inw(x86_port_t port)
+should_inline u16 port_inw(x86_port_t port)
 {
     u16 value;
     __asm__ volatile("inw %1, %0" : "=a"(value) : "dN"(port));
     return value;
 }
 
-always_inline u32 port_inl(x86_port_t port)
+should_inline u32 port_inl(x86_port_t port)
 {
     u32 value;
     __asm__ volatile("inl %1, %0" : "=a"(value) : "dN"(port));
     return value;
 }
 
-always_inline void port_outb(u16 port, u8 value)
+should_inline void port_outb(u16 port, u8 value)
 {
     __asm__ volatile("outb %1, %0" : : "dN"(port), "a"(value));
 }
 
-always_inline void port_outw(x86_port_t port, u16 value)
+should_inline void port_outw(x86_port_t port, u16 value)
 {
     __asm__ volatile("outw %1, %0" : : "dN"(port), "a"(value));
 }
 
-always_inline void port_outl(x86_port_t port, u32 value)
+should_inline void port_outl(x86_port_t port, u32 value)
 {
     __asm__ volatile("outl %1, %0" : : "dN"(port), "a"(value));
 }
