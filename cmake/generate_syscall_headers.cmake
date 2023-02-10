9c4b797ce162bfa860ab59a9ecc86b0ac523d563
Moody Liu <mooodyhunter@outlook.com>
2023-02-10 22:51:46 +0000

    CMake: fix wrong dependencies

--- cmake/generate_syscall_headers.cmake
+++ cmake/generate_syscall_headers.cmake
@@ -5,13 +5,14 @@ find_program(PYTHON "python3" NAMES "python3 python" REQUIRED)
 function(generate_syscall_headers  SYSCALL_JSON)
     make_directory("${CMAKE_BINARY_DIR}/include/mos/syscall/")
 
-    add_library(mos_syscall_decl INTERFACE)
-    add_library(mos::syscall::decl ALIAS mos_syscall_decl)
+    add_custom_target(mos_syscall_decl SOURCES ${SYSCALL_JSON})
 
     foreach(TYPE decl dispatcher number usermode)
         set(OUTPUT_FILE ${CMAKE_BINARY_DIR}/include/mos/syscall/${TYPE}.h)
-        add_custom_command(OUTPUT ${OUTPUT_FILE}
+        add_custom_command(
+            TARGET mos_syscall_decl
             MAIN_DEPENDENCY ${SYSCALL_JSON}
+            BYPRODUCTS ${OUTPUT_FILE}
             DEPENDS ${CMAKE_SOURCE_DIR}/scripts/gen_syscall.py
             COMMAND ${PYTHON}
             ${CMAKE_SOURCE_DIR}/scripts/gen_syscall.py
@@ -20,6 +21,5 @@ function(generate_syscall_headers  SYSCALL_JSON)
             ${OUTPUT_FILE}
             VERBATIM
         )
-        target_sources(mos_syscall_decl INTERFACE ${OUTPUT_FILE})
     endforeach()
 endfunction()
