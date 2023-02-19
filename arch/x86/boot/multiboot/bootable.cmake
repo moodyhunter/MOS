d19a95663663a6c43623dbf0bc914d89339fa253
Moody Liu <mooodyhunter@outlook.com>
2023-02-19 02:44:34 +0000

    fix x86 build

--- arch/x86/boot/multiboot/bootable.cmake
+++ arch/x86/boot/multiboot/bootable.cmake
@@ -3,7 +3,7 @@
 target_sources(mos_kernel PRIVATE ${CMAKE_CURRENT_LIST_DIR}/startup.c)
 
 add_executable(multiboot ${CMAKE_CURRENT_LIST_DIR}/multiboot.asm)
-prepare_bootable_kernel_binary(multiboot LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/multiboot.ld)
+prepare_bootable_kernel_binary(multiboot ${CMAKE_CURRENT_LIST_DIR}/multiboot.ld)
 
 add_custom_command(TARGET multiboot
     POST_BUILD
