997cd6af86f26019d2e788421078225e7d9f8aa0
Moody Liu <mooodyhunter@outlook.com>
2023-02-22 17:05:42 +0000

    cmake : add __MOS_HAS_NO_COMPILER

--- cmake/mos_target_setup.cmake
+++ cmake/mos_target_setup.cmake
@@ -14,12 +14,21 @@ macro(mos_target_setup)
     set(CMAKE_C_COMPILER_LAUNCHER "")
     set(CMAKE_CXX_COMPILER_LAUNCHER "")
 
+    set(CMAKE_C_COMPILER_WORKS 1)
+    set(CMAKE_CXX_COMPILER_WORKS 1)
+
     # set(CMAKE_ASM_NASM_OBJECT_FORMAT elf${BITS})
     # Add debug info to nasm
     set(CMAKE_ASM_NASM_COMPILE_OBJECT "<CMAKE_ASM_NASM_COMPILER> <INCLUDES> -felf${MOS_BITS} -gdwarf -o <OBJECT> <SOURCE>")
 
-    find_program(CMAKE_C_COMPILER NAMES "${MOS_COMPILER_PREFIX}gcc" NO_CACHE REQUIRED)
-    find_program(CMAKE_CXX_COMPILER NAMES "${MOS_COMPILER_PREFIX}g++" NO_CACHE REQUIRED)
+    if (__MOS_HAS_NO_COMPILER)
+        set(CMAKE_C_COMPILER "true")
+        set(CMAKE_CXX_COMPILER "true")
+        message(WARNING "__MOS_HAS_NO_COMPILER is set, using true as compiler.")
+    else()
+        find_program(CMAKE_C_COMPILER NAMES "${MOS_COMPILER_PREFIX}gcc" NO_CACHE "${__REQUIRES}")
+        find_program(CMAKE_CXX_COMPILER NAMES "${MOS_COMPILER_PREFIX}g++" NO_CACHE "${__REQUIRES}")
+    endif()
 
     execute_process(COMMAND ${CMAKE_C_COMPILER} "-print-file-name=crtbegin.o"
         OUTPUT_VARIABLE MOS_CRTBEGIN
@@ -28,16 +37,14 @@ macro(mos_target_setup)
         OUTPUT_VARIABLE MOS_CRTEND
         OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-    set(CMAKE_C_COMPILER_WORKS 1)
-    set(CMAKE_CXX_COMPILER_WORKS 1)
     message(STATUS "Target: C compiler: ${CMAKE_C_COMPILER}")
     message(STATUS "Target: C++ compiler: ${CMAKE_CXX_COMPILER}")
     message(STATUS "Target: CRTBEGIN: ${MOS_CRTBEGIN}, CRTEND: ${MOS_CRTEND}")
 
     # to be used later?
     add_library(mos::crtbegin IMPORTED STATIC GLOBAL)
-    set_target_properties(mos::crtbegin PROPERTIES IMPORTED_LOCATION ${MOS_CRTBEGIN})
+    set_target_properties(mos::crtbegin PROPERTIES IMPORTED_LOCATION "${MOS_CRTBEGIN}")
 
     add_library(mos::crtend IMPORTED STATIC GLOBAL)
-    set_target_properties(mos::crtend PROPERTIES IMPORTED_LOCATION ${MOS_CRTEND})
+    set_target_properties(mos::crtend PROPERTIES IMPORTED_LOCATION "${MOS_CRTEND}")
 endmacro()
