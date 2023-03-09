56f08bf2cfa5c1c0398cb41a5f08c8823f769990
Moody Liu <mooodyhunter@outlook.com>
2023-03-09 00:10:20 +0000

    libipc: add comments

--- userspace/libs/libipc/include/libipc/ipc.h
+++ userspace/libs/libipc/include/libipc/ipc.h
@@ -7,16 +7,47 @@
 #include "mos/syscall/usermode.h"
 #include "mos/types.h"
 
+/**
+ * @brief An IPC message.
+ *
+ */
 typedef struct
 {
     size_t size;
     char data[];
 } ipc_msg_t;
 
+/**
+ * @brief Create a new IPC message.
+ *
+ * @param size The size of the message.
+ * @return ipc_msg_t* The message.
+ */
 ipc_msg_t *ipc_msg_create(size_t size);
+
+/**
+ * @brief Destroy an IPC message.
+ *
+ * @param buffer The message.
+ */
 void ipc_msg_destroy(ipc_msg_t *buffer);
 
+/**
+ * @brief Read an IPC message.
+ *
+ * @param fd The file descriptor.
+ * @return ipc_msg_t* The message.
+ */
 ipc_msg_t *ipc_read_msg(fd_t fd);
+
+/**
+ * @brief Write an IPC message.
+ *
+ * @param fd The file descriptor.
+ * @param buffer The message.
+ * @return true The message was written.
+ * @return false The message was not written.
+ */
 bool ipc_write_msg(fd_t fd, ipc_msg_t *buffer);
 
 size_t ipc_read_as_msg(fd_t fd, char *buffer, size_t buffer_size);
