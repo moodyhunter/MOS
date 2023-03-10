85317e6c383d4fa148bb3b22c34b181f58ad58d7
Moody Liu <mooodyhunter@outlook.com>
2023-03-10 00:52:17 +0000

    coreutils: add shutdown

---
+++ userspace/programs/core-utils/shutdown.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "mos/syscall/usermode.h"
+
+int main(int argc, char **argv)
+{
+    MOS_UNUSED(argc);
+    MOS_UNUSED(argv);
+    syscall_poweroff(false, MOS_FOURCC('G', 'B', 'y', 'e'));
+    return 0;
+}
