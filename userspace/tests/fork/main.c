ef77e939c56221b21c235f9b77042b4db2b3c06a
Moody Liu <mooodyhunter@outlook.com>
2023-03-10 02:19:47 +0000

    fork-test: wait for subprocesses before exiting

--- userspace/tests/fork/main.c
+++ userspace/tests/fork/main.c
@@ -3,27 +3,68 @@
 #include "lib/stdio.h"
 #include "mos/syscall/usermode.h"
 
+#define WAITMSG "pid %ld waits for %ld\n"
+
 int main(void)
 {
     int pid = syscall_fork();
     if (pid == 0)
     {
         printf("Child process: pid = %ld\n", syscall_get_pid());
-        syscall_fork();
-        syscall_fork();
-        syscall_fork();
-        syscall_fork();
-        syscall_fork();
-        syscall_fork();
-        syscall_fork();
+        pid_t p1 = syscall_fork();
+        if (p1)
+        {
+            printf(WAITMSG, syscall_get_pid(), p1);
+            syscall_wait_for_process(p1);
+        }
 
-        printf("after so many forks: pid = %ld\n", syscall_get_pid());
+        pid_t p2 = syscall_fork();
+        if (p2)
+        {
+            printf(WAITMSG, syscall_get_pid(), p2);
+            syscall_wait_for_process(p2);
+        }
+
+        pid_t p3 = syscall_fork();
+        if (p3)
+        {
+            printf(WAITMSG, syscall_get_pid(), p3);
+            syscall_wait_for_process(p3);
+        }
+
+        pid_t p4 = syscall_fork();
+        if (p4)
+        {
+            printf(WAITMSG, syscall_get_pid(), p4);
+            syscall_wait_for_process(p4);
+        }
+
+        pid_t p5 = syscall_fork();
+        if (p5)
+        {
+            printf(WAITMSG, syscall_get_pid(), p5);
+            syscall_wait_for_process(p5);
+        }
+
+        pid_t p6 = syscall_fork();
+        if (p6)
+        {
+            printf(WAITMSG, syscall_get_pid(), p6);
+            syscall_wait_for_process(p6);
+        }
+
+        pid_t p7 = syscall_fork();
+        if (p7)
+        {
+            printf(WAITMSG, syscall_get_pid(), p7);
+            syscall_wait_for_process(p7);
+        }
     }
     else
     {
         printf("Parent process: pid = %ld, child pid = %d\n", syscall_get_pid(), pid);
-        while (1)
-            ;
+        syscall_wait_for_process(pid);
+        printf("fork test passed\n");
     }
     return 0;
 }
