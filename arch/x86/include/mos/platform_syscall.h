ce093835bbdaf2b347cbcacfc9f982197bf1a692
Moody Liu <mooodyhunter@outlook.com>
2023-03-10 17:15:44 +0000

    x86 syscall: add 'memory' to clobbered list

--- arch/x86/include/mos/platform_syscall.h
+++ arch/x86/include/mos/platform_syscall.h
@@ -14,42 +14,42 @@ enum
 should_inline reg_t platform_syscall0(reg_t number)
 {
     reg_t result = 0;
-    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number));
+    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number) : "memory");
     return result;
 }
 
 should_inline reg_t platform_syscall1(reg_t number, reg_t arg1)
 {
     reg_t result = 0;
-    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1));
+    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1) : "memory");
     return result;
 }
 
 should_inline reg_t platform_syscall2(reg_t number, reg_t arg1, reg_t arg2)
 {
     reg_t result = 0;
-    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2));
+    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2) : "memory");
     return result;
 }
 
 should_inline reg_t platform_syscall3(reg_t number, reg_t arg1, reg_t arg2, reg_t arg3)
 {
     reg_t result = 0;
-    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3));
+    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3) : "memory");
     return result;
 }
 
 should_inline reg_t platform_syscall4(reg_t number, reg_t arg1, reg_t arg2, reg_t arg3, reg_t arg4)
 {
     reg_t result = 0;
-    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3), "S"(arg4));
+    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3), "S"(arg4) : "memory");
     return result;
 }
 
 should_inline reg_t platform_syscall5(reg_t number, reg_t arg1, reg_t arg2, reg_t arg3, reg_t arg4, reg_t arg5)
 {
     reg_t result = 0;
-    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3), "S"(arg4), "D"(arg5));
+    __asm__ volatile("int $0x88" : "=a"(result) : "a"(number), "b"(arg1), "c"(arg2), "d"(arg3), "S"(arg4), "D"(arg5) : "memory");
     return result;
 }
 
@@ -76,6 +76,7 @@ should_inline reg_t platform_syscall6(reg_t number, reg_t arg1, reg_t arg2, reg_
                      "pop %%edi\n"
                      "pop %%ebx\n"
                      : "=a"(result)
-                     : "m"(number), "m"(arg1), "m"(arg2), "m"(arg3), "m"(arg4), "m"(arg5), "m"(arg6));
+                     : "m"(number), "m"(arg1), "m"(arg2), "m"(arg3), "m"(arg4), "m"(arg5), "m"(arg6)
+                     : "memory");
     return result;
 }
