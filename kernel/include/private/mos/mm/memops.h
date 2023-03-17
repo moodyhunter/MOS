429ed791a835b37cace3fa23658176e3bb660b56
Moody Liu <mooodyhunter@outlook.com>
2023-03-17 00:42:40 +0000

    memops: remove unused functions

--- kernel/include/private/mos/mm/memops.h
+++ kernel/include/private/mos/mm/memops.h
@@ -6,8 +6,3 @@
 
 vmblock_t mm_alloc_zeroed_pages(paging_handle_t handle, size_t npages, pgalloc_hints hints, vm_flags flags);
 vmblock_t mm_alloc_zeroed_pages_at(paging_handle_t handle, uintptr_t vaddr, size_t npages, vm_flags flags);
-
-vmblock_t mm_map_proxy_space(paging_handle_t from, uintptr_t fvaddr, size_t npages);
-void mm_unmap_proxy_space(vmblock_t proxy_blk);
-
-void mm_copy_pages(paging_handle_t from, uintptr_t src, paging_handle_t to, uintptr_t dst, size_t n_pages);
