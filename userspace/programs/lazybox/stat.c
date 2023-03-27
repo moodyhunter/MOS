fe1c1530acf22eb21ee2a98726812c39df3fc430
Moody Liu <mooodyhunter@outlook.com>
2023-03-27 01:42:44 +0100

    lazybox: add stat

---
+++ userspace/programs/lazybox/stat.c
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "mos/moslib_global.h"
+
+#include <mos/filesystem/fs_types.h>
+#include <mos/syscall/usermode.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        dprintf(stderr, "Usage: %s <path>...\n", argv[0]);
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        file_stat_t stat;
+        if (!syscall_vfs_stat(argv[i], &stat))
+        {
+            dprintf(stderr, "%s: No such file or directory\n", argv[i]);
+            return 1;
+        }
+
+        printf("File: %s\n", argv[i]);
+        printf("File size: %zd bytes\n", stat.size);
+        switch (stat.type)
+        {
+            case FILE_TYPE_REGULAR: printf("Type: Regular file\n"); break;
+            case FILE_TYPE_DIRECTORY: printf("Type: Directory\n"); break;
+            case FILE_TYPE_CHAR_DEVICE: printf("Type: Character device\n"); break;
+            case FILE_TYPE_BLOCK_DEVICE: printf("Type: Block device\n"); break;
+            case FILE_TYPE_NAMED_PIPE: printf("Type: Pipe\n"); break;
+            case FILE_TYPE_SOCKET: printf("Type: Socket\n"); break;
+            case FILE_TYPE_SYMLINK: printf("Type: Symbolic link\n"); break;
+            default: printf("Type: Unknown\n"); break;
+        }
+
+        printf("Owner: %ld:%ld\n", stat.uid, stat.gid);
+
+        char buf[16] = { 0 };
+        file_format_perm(stat.perm, buf);
+        printf("Permissions: %.9s", buf);
+
+        if (stat.suid)
+            printf("[SUID]");
+        if (stat.sgid)
+            printf("[SGID]");
+        if (stat.sticky)
+            printf("[STICKY]");
+        printf("\n");
+    }
+
+    return 0;
+}
