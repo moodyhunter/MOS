7fe431c59d708c66a186152098193229dcb6e08f
Moody Liu <mooodyhunter@outlook.com>
2023-04-26 16:52:15 +0100

    mm: add mm_make_cow_block wrapper

--- kernel/include/private/mos/mm/cow.h
+++ kernel/include/private/mos/mm/cow.h
@@ -5,4 +5,5 @@
 #include <mos/platform/platform.h>
 
 vmblock_t mm_make_cow(paging_handle_t from, ptr_t fvaddr, paging_handle_t to, ptr_t tvaddr, size_t npages, vm_flags flags);
+vmblock_t mm_make_cow_block(paging_handle_t target_handle, vmblock_t src_block);
 bool mm_handle_pgfault(ptr_t fault_addr, bool present, bool is_write, bool is_user, bool is_exec);
--- kernel/mm/cow.c
+++ kernel/mm/cow.c
@@ -11,6 +11,11 @@
 #include <mos/tasks/task_types.h>
 #include <string.h>
 
+vmblock_t mm_make_cow_block(paging_handle_t target_handle, vmblock_t src_block)
+{
+    return mm_make_cow(src_block.address_space, src_block.vaddr, target_handle, src_block.vaddr, src_block.npages, src_block.flags);
+}
+
 vmblock_t mm_make_cow(paging_handle_t from, ptr_t fvaddr, paging_handle_t to, ptr_t tvaddr, size_t npages, vm_flags flags)
 {
     // Note that the block returned by this function contains it's ACTRUAL flags, which may be different from the flags of the original block.
