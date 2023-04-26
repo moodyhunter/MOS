9cf5be9de51170b9f63b93f4425ccdd3c198c29f
Moody Liu <mooodyhunter@outlook.com>
2023-04-26 16:52:37 +0100

    fork: refactor fork() impl

--- kernel/tasks/fork.c
+++ kernel/tasks/fork.c
@@ -13,86 +13,87 @@
 #include <mos/tasks/thread.h>
 #include <string.h>
 
+#define FORKFMT "fork %ld->%ld: %10s " PTR_FMT "+%-3zu flags [0x%x]"
+
 process_t *process_handle_fork(process_t *parent)
 {
     MOS_ASSERT(process_is_valid(parent));
 
-    process_t *child = process_allocate(parent, parent->name);
-    if (unlikely(!child))
+    process_t *child_p = process_allocate(parent, parent->name);
+    if (unlikely(!child_p))
     {
         pr_emerg("failed to allocate process for fork");
         return NULL;
     }
 
-    pr_emph("process %ld forked to %ld", parent->pid, child->pid);
+    pr_emph("process %ld forked to %ld", parent->pid, child_p->pid);
 
     // copy the parent's memory
     for (size_t i = 0; i < parent->mmaps_count; i++)
     {
-        vmap_t *block = &parent->mmaps[i];
-        vmblock_t child_vmblock;
-
-#define FORKFMT "fork %ld->%ld: %10s " PTR_FMT "+%-3zu flags [0x%x]"
-
-        if (block->content == VMTYPE_KSTACK)
+        vmap_t *vmap = &parent->mmaps[i];
+        switch (vmap->content)
         {
-            // Kernel stacks are special, we need to allocate a new one (not CoW-mapped)
-            MOS_ASSERT_X(block->blk.npages == MOS_STACK_PAGES_KERNEL, "kernel stack size is not %d pages", MOS_STACK_PAGES_KERNEL);
-            child_vmblock = mm_alloc_pages(child->pagetable, block->blk.npages, MOS_ADDR_USER_STACK, VALLOC_DEFAULT, block->blk.flags);
-            pr_info2(FORKFMT, parent->pid, child->pid, "kstack", block->blk.vaddr, block->blk.npages, block->blk.flags);
-            process_attach_mmap(child, child_vmblock, VMTYPE_KSTACK, (vmap_flags_t){ 0 });
-            continue;
-        }
-
-        if (block->content == VMTYPE_MMAP || block->content == VMTYPE_FILE)
-        {
-            MOS_ASSERT_X(block->flags.fork_mode != VMAP_FORK_NA, "invalid fork mode for mmap");
-            if (block->flags.fork_mode == VMAP_FORK_SHARED)
+            case VMTYPE_KSTACK:
             {
-                pr_info2(FORKFMT, parent->pid, child->pid, "mmap", block->blk.vaddr, block->blk.npages, block->blk.flags);
-                process_attach_mmap(child, block->blk, block->content, block->flags);
-                continue;
+                // Kernel stacks are special, we need to allocate a new one (not CoW-mapped)
+                MOS_ASSERT_X(vmap->blk.npages == MOS_STACK_PAGES_KERNEL, "kernel stack size is not %d pages", MOS_STACK_PAGES_KERNEL);
+                const vmblock_t block = mm_alloc_pages(child_p->pagetable, vmap->blk.npages, MOS_ADDR_USER_STACK, VALLOC_DEFAULT, vmap->blk.flags);
+
+                pr_info2(FORKFMT, parent->pid, child_p->pid, "kstack", vmap->blk.vaddr, vmap->blk.npages, vmap->blk.flags);
+                process_attach_mmap(child_p, block, VMTYPE_KSTACK, (vmap_flags_t){ 0 });
+                break;
+            }
+
+            case VMTYPE_FILE:
+            case VMTYPE_MMAP:
+            {
+                MOS_ASSERT_X(vmap->flags.fork_mode != VMAP_FORK_NA, "invalid fork mode for mmap");
+                if (vmap->flags.fork_mode == VMAP_FORK_SHARED)
+                {
+                    pr_info2(FORKFMT, parent->pid, child_p->pid, "mmap", vmap->blk.vaddr, vmap->blk.npages, vmap->blk.flags);
+                    process_attach_mmap(child_p, vmap->blk, vmap->content, vmap->flags);
+                    break;
+                }
+
+                [[fallthrough]]; // private mmap, fall through to CoW
+            }
+            case VMTYPE_CODE:
+            case VMTYPE_DATA:
+            case VMTYPE_HEAP:
+            case VMTYPE_STACK:
+            {
+                vmap->flags.cow = true;
+                const vmblock_t block = mm_make_cow_block(child_p->pagetable, vmap->blk);
+
+                pr_info2(FORKFMT, parent->pid, child_p->pid, "CoW", vmap->blk.vaddr, vmap->blk.npages, vmap->blk.flags);
+                process_attach_mmap(child_p, block, vmap->content, vmap->flags);
+                break;
             }
         }
-
-        block->flags.cow = true;
-        pr_info2(FORKFMT, parent->pid, child->pid, "CoW", block->blk.vaddr, block->blk.npages, block->blk.flags);
-        mm_make_cow(parent->pagetable, block->blk.vaddr, child->pagetable, block->blk.vaddr, block->blk.npages, block->blk.flags);
-        child_vmblock = parent->mmaps[i].blk; // do not use the return value from mm_make_process_map_cow
-        child_vmblock.address_space = child->pagetable;
-        process_attach_mmap(child, child_vmblock, block->content, block->flags);
     }
 
     // copy the parent's files
     for (int i = 0; i < MOS_PROCESS_MAX_OPEN_FILES; i++)
     {
         io_t *file = parent->files[i];
-        if (!file)
-            continue; // skip empty slots
-        if (file->closed)
-            continue; // skip closed files
-        child->files[i] = io_ref(file);
+        if (!file || file->closed)
+            continue; // skip empty slots and closed files
+        child_p->files[i] = io_ref(file);
     }
 
     // copy the parent's threads
-    for (int i = 0; i < parent->threads_count; i++)
-    {
-        thread_t *parent_thread = parent->threads[i];
-        if (parent_thread != current_thread)
-            continue;
+    const thread_t *parent_thread = current_thread;
+    thread_t *child_t = thread_allocate(child_p, parent_thread->mode);
+    child_t->u_stack = parent_thread->u_stack;
+    child_t->k_stack = parent_thread->k_stack;
+    child_t->name = strdup(parent_thread->name);
+    pr_info2("fork: thread %ld->%ld", parent_thread->tid, child_t->tid);
+    platform_setup_forked_context(parent_thread->context, &child_t->context);
+    process_attach_thread(child_p, child_t);
 
-        thread_t *child_thread = thread_allocate(child, parent_thread->mode);
-        child_thread->u_stack = parent_thread->u_stack;
-        child_thread->k_stack = parent_thread->k_stack;
-        child_thread->name = strdup(parent_thread->name);
-        child_thread->state = THREAD_STATE_CREATED;
-        pr_info2("fork: thread %ld->%ld", parent_thread->tid, child_thread->tid);
-        platform_setup_forked_context(parent_thread->context, &child_thread->context);
-
-        process_attach_thread(child, child_thread);
-        hashmap_put(thread_table, child_thread->tid, child_thread);
-    }
-
-    hashmap_put(process_table, child->pid, child);
-    return child;
+    hashmap_put(thread_table, child_t->tid, child_t);
+    hashmap_put(process_table, child_p->pid, child_p);
+    thread_setup_complete(child_t);
+    return child_p;
 }
