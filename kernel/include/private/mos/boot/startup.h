7886c010abca46651401cf213ccad94b2af377c6
Moody Liu <mooodyhunter@outlook.com>
2023-04-29 03:16:45 +0100

    startup: use should_inline instead of always_inline

--- kernel/include/private/mos/boot/startup.h
+++ kernel/include/private/mos/boot/startup.h
@@ -14,14 +14,14 @@
 // vaddr and paddr must be page-aligned
 __startup_code void mos_startup_map_single_page(ptr_t vaddr, ptr_t paddr, vm_flags flags);
 
-__startup_code always_inline void mos_startup_memzero(void *start, size_t size)
+__startup_code should_inline void mos_startup_memzero(void *start, size_t size)
 {
     u8 *ptr = start;
     for (size_t i = 0; i < size; i++)
         ptr[i] = 0;
 }
 
-__startup_code always_inline size_t mos_startup_strlen(const char *str)
+__startup_code should_inline size_t mos_startup_strlen(const char *str)
 {
     size_t len = 0;
     while (str[len])
@@ -29,13 +29,13 @@ __startup_code always_inline size_t mos_startup_strlen(const char *str)
     return len;
 }
 
-__startup_code always_inline void mos_startup_map_pages(ptr_t vaddr, ptr_t paddr, size_t npages, vm_flags flags)
+__startup_code should_inline void mos_startup_map_pages(ptr_t vaddr, ptr_t paddr, size_t npages, vm_flags flags)
 {
     for (size_t i = 0; i < npages; i++)
         mos_startup_map_single_page(vaddr + i * MOS_PAGE_SIZE, paddr + i * MOS_PAGE_SIZE, flags);
 }
 
-__startup_code always_inline void mos_startup_map_bytes(ptr_t vaddr, ptr_t paddr, size_t nbytes, vm_flags flags)
+__startup_code should_inline void mos_startup_map_bytes(ptr_t vaddr, ptr_t paddr, size_t nbytes, vm_flags flags)
 {
     paddr = ALIGN_DOWN_TO_PAGE(paddr);
     const ptr_t aligned_vaddr = ALIGN_DOWN_TO_PAGE(vaddr);
@@ -43,13 +43,13 @@ __startup_code always_inline void mos_startup_map_bytes(ptr_t vaddr, ptr_t paddr
     mos_startup_map_pages(aligned_vaddr, paddr, npages, flags);
 }
 
-__startup_code always_inline void mos_startup_map_identity(ptr_t paddr, size_t nbytes, vm_flags flags)
+__startup_code should_inline void mos_startup_map_identity(ptr_t paddr, size_t nbytes, vm_flags flags)
 {
     paddr = ALIGN_DOWN_TO_PAGE(paddr);
     mos_startup_map_bytes(paddr, paddr, nbytes, flags);
 }
 
-__startup_code always_inline void mos_startup_map_bios(ptr_t paddr, size_t nbytes, vm_flags flags)
+__startup_code should_inline void mos_startup_map_bios(ptr_t paddr, size_t nbytes, vm_flags flags)
 {
     paddr = ALIGN_DOWN_TO_PAGE(paddr);
     mos_startup_map_bytes(BIOS_VADDR(paddr), paddr, nbytes, flags | VM_CACHE_DISABLED);
