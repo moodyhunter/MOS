a8ac7a8de11efe72c8b2ec89abc86ba1bb6a4c22
Moody Liu <mooodyhunter@outlook.com>
2023-08-08 21:32:40 +0800

    x86/platform_defs: fix typo

--- arch/x86_64/include/private/mos/platform/platform_defs.h
+++ arch/x86_64/include/private/mos/platform/platform_defs.h
@@ -13,14 +13,14 @@
 #define PML2_SHIFT        21
 #define PML2_MASK         0x1FFL // 9 bits page directory offset
 #define PML2_ENTRIES      512
-#define PML2_HUGE_CAPABLE 1
+#define PML2_HUGE_CAPABLE 1 // 2MB pages
 
 #define PML3_SHIFT        30
 #define PML3_MASK         0x1FFL // 9 bits page directory pointer offset
 #define PML3_ENTRIES      512
-#define PML3_HUGE_CAPABLE 1
+#define PML3_HUGE_CAPABLE 1 // 1GB pages
 
 #define PML4_SHIFT        39
 #define PML4_MASK         0x1FFL // 9 bits page map level 4 offset
 #define PML4_ENTRIES      512
-#define PML4_HUGE_CAPABLE -1 // we don't support 1GiB pages (for now)
+#define PML4_HUGE_CAPABLE -1
