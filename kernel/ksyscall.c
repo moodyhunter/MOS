dbb06fcd00298f75f819eadf6b1479a41184f345
Moody Liu <mooodyhunter@outlook.com>
2023-08-14 17:08:07 +0800

    syscall: symbol-safe syscall impl

--- kernel/ksyscall.c
+++ kernel/ksyscall.c
@@ -13,6 +13,7 @@
 #include <mos/platform/platform.h>
 #include <mos/printk.h>
 #include <mos/syscall/decl.h>
+#include <mos/syscall/number.h>
 #include <mos/tasks/process.h>
 #include <mos/tasks/schedule.h>
 #include <mos/tasks/task_types.h>
@@ -20,7 +21,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-void define_syscall(poweroff)(bool reboot, u32 magic)
+#define DEFINE_SYSCALL(ret, name)                                                                                                                                        \
+    MOS_STATIC_ASSERT(SYSCALL_DEFINED(name));                                                                                                                            \
+    ret define_syscall(name)
+
+DEFINE_SYSCALL(void, poweroff)(bool reboot, u32 magic)
 {
 #define POWEROFF_MAGIC MOS_FOURCC('G', 'B', 'y', 'e')
     if (magic != POWEROFF_MAGIC)
@@ -40,7 +45,7 @@ void define_syscall(poweroff)(bool reboot, u32 magic)
     }
 }
 
-fd_t define_syscall(vfs_openat)(fd_t dirfd, const char *path, open_flags flags)
+DEFINE_SYSCALL(fd_t, vfs_openat)(fd_t dirfd, const char *path, open_flags flags)
 {
     if (path == NULL)
         return -1;
@@ -51,7 +56,7 @@ fd_t define_syscall(vfs_openat)(fd_t dirfd, const char *path, open_flags flags)
     return process_attach_ref_fd(current_process, &f->io);
 }
 
-bool define_syscall(vfs_fstatat)(fd_t fd, const char *path, file_stat_t *stat_buf, fstatat_flags flags)
+DEFINE_SYSCALL(bool, vfs_fstatat)(fd_t fd, const char *path, file_stat_t *stat_buf, fstatat_flags flags)
 {
     if (fd < 0)
         return false;
@@ -59,7 +64,7 @@ bool define_syscall(vfs_fstatat)(fd_t fd, const char *path, file_stat_t *stat_bu
     return vfs_fstatat(fd, path, stat_buf, flags);
 }
 
-size_t define_syscall(io_read)(fd_t fd, void *buf, size_t count)
+DEFINE_SYSCALL(size_t, io_read)(fd_t fd, void *buf, size_t count)
 {
     if (fd < 0 || buf == NULL)
         return -1;
@@ -71,7 +76,7 @@ size_t define_syscall(io_read)(fd_t fd, void *buf, size_t count)
     return io_read(io, buf, count);
 }
 
-size_t define_syscall(io_write)(fd_t fd, const void *buf, size_t count)
+DEFINE_SYSCALL(size_t, io_write)(fd_t fd, const void *buf, size_t count)
 {
     if (fd < 0 || buf == NULL)
     {
@@ -88,7 +93,7 @@ size_t define_syscall(io_write)(fd_t fd, const void *buf, size_t count)
     return io_write(io, buf, count);
 }
 
-bool define_syscall(io_close)(fd_t fd)
+DEFINE_SYSCALL(bool, io_close)(fd_t fd)
 {
     if (fd < 0)
         return false;
@@ -96,7 +101,7 @@ bool define_syscall(io_close)(fd_t fd)
     return true;
 }
 
-noreturn void define_syscall(exit)(u32 exit_code)
+DEFINE_SYSCALL(noreturn void, exit)(u32 exit_code)
 {
     const pid_t pid = current_process->pid;
     if (unlikely(pid == 1))
@@ -105,12 +110,12 @@ noreturn void define_syscall(exit)(u32 exit_code)
     process_handle_exit(current_process, exit_code);
 }
 
-void define_syscall(yield_cpu)(void)
+DEFINE_SYSCALL(void, yield_cpu)(void)
 {
     reschedule();
 }
 
-pid_t define_syscall(fork)(void)
+DEFINE_SYSCALL(pid_t, fork)(void)
 {
     process_t *parent = current_process;
     process_t *child = process_handle_fork(parent);
@@ -119,7 +124,7 @@ pid_t define_syscall(fork)(void)
     return child->pid; // return 0 for child, pid for parent
 }
 
-pid_t define_syscall(exec)(const char *path, const char *const argv[])
+DEFINE_SYSCALL(pid_t, exec)(const char *path, const char *const argv[])
 {
     MOS_UNUSED(path);
     MOS_UNUSED(argv);
@@ -127,17 +132,17 @@ pid_t define_syscall(exec)(const char *path, const char *const argv[])
     return -1;
 }
 
-pid_t define_syscall(get_pid)(void)
+DEFINE_SYSCALL(pid_t, get_pid)(void)
 {
     return current_process->pid;
 }
 
-pid_t define_syscall(get_parent_pid)(void)
+DEFINE_SYSCALL(pid_t, get_parent_pid)(void)
 {
     return current_process->parent->pid;
 }
 
-pid_t define_syscall(spawn)(const char *path, int argc, const char *const argv[])
+DEFINE_SYSCALL(pid_t, spawn)(const char *path, int argc, const char *const argv[])
 {
     process_t *current = current_process;
 
@@ -159,7 +164,7 @@ pid_t define_syscall(spawn)(const char *path, int argc, const char *const argv[]
     return process->pid;
 }
 
-tid_t define_syscall(create_thread)(const char *name, thread_entry_t entry, void *arg)
+DEFINE_SYSCALL(tid_t, create_thread)(const char *name, thread_entry_t entry, void *arg)
 {
     thread_t *thread = thread_new(current_process, THREAD_MODE_USER, name, entry, arg);
     if (thread == NULL)
@@ -169,17 +174,17 @@ tid_t define_syscall(create_thread)(const char *name, thread_entry_t entry, void
     return thread->tid;
 }
 
-tid_t define_syscall(get_tid)(void)
+DEFINE_SYSCALL(tid_t, get_tid)(void)
 {
     return current_thread->tid;
 }
 
-noreturn void define_syscall(thread_exit)(void)
+DEFINE_SYSCALL(noreturn void, thread_exit)(void)
 {
     thread_handle_exit(current_thread);
 }
 
-ptr_t define_syscall(heap_control)(heap_control_op op, ptr_t arg)
+DEFINE_SYSCALL(ptr_t, heap_control)(heap_control_op op, ptr_t arg)
 {
     process_t *process = current_process;
 
@@ -239,22 +244,22 @@ ptr_t define_syscall(heap_control)(heap_control_op op, ptr_t arg)
     }
 }
 
-bool define_syscall(wait_for_thread)(tid_t tid)
+DEFINE_SYSCALL(bool, wait_for_thread)(tid_t tid)
 {
     return thread_wait_for_tid(tid);
 }
 
-bool define_syscall(futex_wait)(futex_word_t *futex, u32 val)
+DEFINE_SYSCALL(bool, futex_wait)(futex_word_t *futex, u32 val)
 {
     return futex_wait(futex, val);
 }
 
-bool define_syscall(futex_wake)(futex_word_t *futex, size_t count)
+DEFINE_SYSCALL(bool, futex_wake)(futex_word_t *futex, size_t count)
 {
     return futex_wake(futex, count);
 }
 
-fd_t define_syscall(ipc_create)(const char *name, size_t max_pending_connections)
+DEFINE_SYSCALL(fd_t, ipc_create)(const char *name, size_t max_pending_connections)
 {
     io_t *io = ipc_create(name, max_pending_connections);
     if (io == NULL)
@@ -262,7 +267,7 @@ fd_t define_syscall(ipc_create)(const char *name, size_t max_pending_connections
     return process_attach_ref_fd(current_process, io);
 }
 
-fd_t define_syscall(ipc_accept)(fd_t listen_fd)
+DEFINE_SYSCALL(fd_t, ipc_accept)(fd_t listen_fd)
 {
     io_t *server = process_get_fd(current_process, listen_fd);
     if (server == NULL)
@@ -275,7 +280,7 @@ fd_t define_syscall(ipc_accept)(fd_t listen_fd)
     return process_attach_ref_fd(current_process, client_io);
 }
 
-fd_t define_syscall(ipc_connect)(const char *server, size_t buffer_size)
+DEFINE_SYSCALL(fd_t, ipc_connect)(const char *server, size_t buffer_size)
 {
     io_t *io = ipc_connect(server, buffer_size);
     if (io == NULL)
@@ -283,37 +288,37 @@ fd_t define_syscall(ipc_connect)(const char *server, size_t buffer_size)
     return process_attach_ref_fd(current_process, io);
 }
 
-u64 define_syscall(arch_syscall)(u64 syscall, u64 arg1, u64 arg2, u64 arg3, u64 arg4)
+DEFINE_SYSCALL(u64, arch_syscall)(u64 syscall, u64 arg1, u64 arg2, u64 arg3, u64 arg4)
 {
     return platform_arch_syscall(syscall, arg1, arg2, arg3, arg4);
 }
 
-bool define_syscall(vfs_mount)(const char *device, const char *mountpoint, const char *fs_type, const char *options)
+DEFINE_SYSCALL(bool, vfs_mount)(const char *device, const char *mountpoint, const char *fs_type, const char *options)
 {
     return vfs_mount(device, mountpoint, fs_type, options);
 }
 
-ssize_t define_syscall(vfs_readlinkat)(fd_t dirfd, const char *path, char *buf, size_t buflen)
+DEFINE_SYSCALL(ssize_t, vfs_readlinkat)(fd_t dirfd, const char *path, char *buf, size_t buflen)
 {
     return vfs_readlinkat(dirfd, path, buf, buflen);
 }
 
-bool define_syscall(vfs_touch)(const char *path, file_type_t type, u32 mode)
+DEFINE_SYSCALL(bool, vfs_touch)(const char *path, file_type_t type, u32 mode)
 {
     return vfs_touch(path, type, mode);
 }
 
-bool define_syscall(vfs_symlink)(const char *target, const char *linkpath)
+DEFINE_SYSCALL(bool, vfs_symlink)(const char *target, const char *linkpath)
 {
     return vfs_symlink(target, linkpath);
 }
 
-bool define_syscall(vfs_mkdir)(const char *path)
+DEFINE_SYSCALL(bool, vfs_mkdir)(const char *path)
 {
     return vfs_mkdir(path);
 }
 
-size_t define_syscall(vfs_list_dir)(fd_t fd, char *buffer, size_t buffer_size)
+DEFINE_SYSCALL(size_t, vfs_list_dir)(fd_t fd, char *buffer, size_t buffer_size)
 {
     io_t *io = process_get_fd(current_process, fd);
     if (io == NULL)
@@ -321,7 +326,7 @@ size_t define_syscall(vfs_list_dir)(fd_t fd, char *buffer, size_t buffer_size)
     return vfs_list_dir(io, buffer, buffer_size);
 }
 
-void *define_syscall(mmap_anonymous)(ptr_t hint_addr, size_t size, mem_perm_t perm, mmap_flags_t flags)
+DEFINE_SYSCALL(void *, mmap_anonymous)(ptr_t hint_addr, size_t size, mem_perm_t perm, mmap_flags_t flags)
 {
     const vm_flags vmflags = VM_USER | (vm_flags) perm; // vm_flags shares the same values as mem_perm_t
     const size_t n_pages = ALIGN_DOWN_TO_PAGE(size) / MOS_PAGE_SIZE;
@@ -330,7 +335,7 @@ void *define_syscall(mmap_anonymous)(ptr_t hint_addr, size_t size, mem_perm_t pe
     return (void *) result;
 }
 
-void *define_syscall(mmap_file)(ptr_t hint_addr, size_t size, mem_perm_t perm, mmap_flags_t mmap_flags, fd_t fd, off_t offset)
+DEFINE_SYSCALL(void *, mmap_file)(ptr_t hint_addr, size_t size, mem_perm_t perm, mmap_flags_t mmap_flags, fd_t fd, off_t offset)
 {
     const vm_flags vmflags = VM_USER | (vm_flags) perm; // vm_flags shares the same values as mem_perm_t
     const size_t n_pages = ALIGN_DOWN_TO_PAGE(size) / MOS_PAGE_SIZE;
@@ -343,27 +348,27 @@ void *define_syscall(mmap_file)(ptr_t hint_addr, size_t size, mem_perm_t perm, m
     return (void *) result;
 }
 
-bool define_syscall(wait_for_process)(pid_t pid)
+DEFINE_SYSCALL(bool, wait_for_process)(pid_t pid)
 {
     return process_wait_for_pid(pid);
 }
 
-bool define_syscall(munmap)(void *addr, size_t size)
+DEFINE_SYSCALL(bool, munmap)(void *addr, size_t size)
 {
     return munmap((ptr_t) addr, size);
 }
 
-bool define_syscall(vfs_chdir)(const char *path)
+DEFINE_SYSCALL(bool, vfs_chdir)(const char *path)
 {
     return vfs_chdir(path);
 }
 
-ssize_t define_syscall(vfs_getcwd)(char *buf, size_t size)
+DEFINE_SYSCALL(ssize_t, vfs_getcwd)(char *buf, size_t size)
 {
     return vfs_getcwd(buf, size);
 }
 
-off_t define_syscall(io_seek)(fd_t fd, off_t offset, io_seek_whence_t whence)
+DEFINE_SYSCALL(off_t, io_seek)(fd_t fd, off_t offset, io_seek_whence_t whence)
 {
     io_t *io = process_get_fd(current_process, fd);
     if (io == NULL)
@@ -371,7 +376,7 @@ off_t define_syscall(io_seek)(fd_t fd, off_t offset, io_seek_whence_t whence)
     return io_seek(io, offset, whence);
 }
 
-off_t define_syscall(io_tell)(fd_t fd)
+DEFINE_SYSCALL(off_t, io_tell)(fd_t fd)
 {
     io_t *io = process_get_fd(current_process, fd);
     if (io == NULL)
@@ -379,12 +384,12 @@ off_t define_syscall(io_tell)(fd_t fd)
     return io_tell(io);
 }
 
-bool define_syscall(signal_register)(signal_t sig, sigaction_t *action)
+DEFINE_SYSCALL(bool, signal_register)(signal_t sig, sigaction_t *action)
 {
     return process_register_signal_handler(current_process, sig, action);
 }
 
-bool define_syscall(signal)(pid_t pid, signal_t sig)
+DEFINE_SYSCALL(bool, signal_process)(pid_t pid, signal_t sig)
 {
     // stub
     MOS_UNUSED(pid);
@@ -392,7 +397,7 @@ bool define_syscall(signal)(pid_t pid, signal_t sig)
     return false;
 }
 
-bool define_syscall(signal_thread)(tid_t tid, signal_t sig)
+DEFINE_SYSCALL(bool, signal_thread)(tid_t tid, signal_t sig)
 {
     thread_t *thread = thread_get(tid);
     if (thread == NULL)
@@ -401,7 +406,7 @@ bool define_syscall(signal_thread)(tid_t tid, signal_t sig)
     return true;
 }
 
-noreturn void define_syscall(signal_return)(void *sp)
+DEFINE_SYSCALL(noreturn void, signal_return)(void *sp)
 {
     signal_return(sp);
 }
