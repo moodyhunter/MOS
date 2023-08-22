44aa74532bb4f5dc68b86c19668a62ef25d69603
Moody Liu <mooodyhunter@outlook.com>
2023-08-22 23:55:27 +0800

    sysfs: add its own debug switch

--- kernel/filesystem/sysfs/sysfs.c
+++ kernel/filesystem/sysfs/sysfs.c
@@ -40,7 +40,7 @@ void sysfs_register(sysfs_dir_t *dir)
 {
     linked_list_init(list_node(dir));
     list_node_append(&sysfs_dirs, list_node(dir));
-    pr_info2("registering '%s'", dir->name);
+    mos_debug(sysfs, "registering '%s'", dir->name);
     MOS_ASSERT(sysfs_sb);
     sysfs_do_register(dir);
 }
@@ -107,7 +107,7 @@ void *sysfs_file_get_data(sysfs_file_t *file)
 
 static bool sysfs_fops_open(inode_t *i, file_t *file)
 {
-    mos_debug(vfs, "opening %s in %s", file->dentry->name, dentry_parent(file->dentry)->name);
+    mos_debug(sysfs, "opening %s in %s", file->dentry->name, dentry_parent(file->dentry)->name);
     sysfs_file_t *f = i->private;
     f->buf_page = NULL;
     f->buf_npages = 0;
@@ -117,7 +117,7 @@ static bool sysfs_fops_open(inode_t *i, file_t *file)
 
 static void sysfs_fops_release(file_t *file)
 {
-    mos_debug(vfs, "closing %s in %s", file->dentry->name, dentry_parent(file->dentry)->name);
+    mos_debug(sysfs, "closing %s in %s", file->dentry->name, dentry_parent(file->dentry)->name);
     sysfs_file_t *f = file->dentry->inode->private;
     if (f->buf_page)
         pmm_unref(f->buf_page, f->buf_npages);
