41e9627176c5d62680a715d4c4dc749baa8dbe84
Moody Liu <mooodyhunter@outlook.com>
2023-08-23 13:26:29 +0800

    acpi-daemon: cleanup

--- userspace/drivers/acpi-daemon/main.c
+++ userspace/drivers/acpi-daemon/main.c
@@ -14,9 +14,6 @@ int main(int argc, const char *argv[])
     MOS_UNUSED(argv);
     puts("ACPI daemon started.");
 
-    if (syscall_vfs_mkdir("/sys"))
-        syscall_vfs_mount("none", "/sys", "sysfs", "defaults");
-
     syscall_arch_syscall(X86_SYSCALL_IOPL_ENABLE, 0, 0, 0, 0);
 
     lai_create_namespace();
