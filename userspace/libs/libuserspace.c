fa915e460f63e49995d52ee8090a8611296a5aef
Moody Liu <mooodyhunter@outlook.com>
2023-08-23 13:24:38 +0800

    stdlib: fix fatal_abort

--- userspace/libs/libuserspace.c
+++ userspace/libs/libuserspace.c
@@ -61,7 +61,7 @@ void fatal_abort(const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
-    fprintf(stderr, fmt, ap);
+    vfprintf(stderr, fmt, ap);
     va_end(ap);
     abort();
     while (1)
