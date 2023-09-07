f743b14e1f6efa5d89b01417b527b01b686526bc
Moody Liu <mooodyhunter@outlook.com>
2023-09-07 15:36:29 +0800

    ci: use mos-specfic compiler with Arch Linux container

--- arch/x86_64/platform_config.cmake
+++ arch/x86_64/platform_config.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-set(MOS_COMPILER_PREFIX "x86_64-elf-")
+set(MOS_COMPILER_PREFIX "x86_64-mos-")
 set(MOS_CX_FLAGS "${MOS_CX_FLAGS} -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx")
 set(MOS_KERNEL_CFLAGS "-mno-red-zone;-mcmodel=kernel")
 set(MOS_BITS 64)
