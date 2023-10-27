d0603b841d9b8fa781d9a84176281eb7e3270987
Moody Liu <mooodyhunter@outlook.com>
2023-10-28 00:08:48 +0100

    lprintk: add a magic macro

--- kernel/include/private/mos/printk.h
+++ kernel/include/private/mos/printk.h
@@ -56,7 +56,7 @@ typedef enum
 
 #if MOS_CONFIG(MOS_PRINTK_WITH_DATETIME)
 #define _lprintk_datetime_fmt "%s | "
-#define _lprintk_datetime_arg , (const char *) platform_get_datetime_str()
+#define _lprintk_datetime_arg (const char *) platform_get_datetime_str()
 #else
 #define _lprintk_datetime_fmt ""
 #define _lprintk_datetime_arg
@@ -64,7 +64,7 @@ typedef enum
 
 #if MOS_CONFIG(MOS_PRINTK_WITH_CPU_ID)
 #define _lprintk_cpuid_fmt "cpu %2d | "
-#define _lprintk_cpuid_arg , platform_current_cpu_id()
+#define _lprintk_cpuid_arg platform_current_cpu_id()
 #else
 #define _lprintk_cpuid_fmt ""
 #define _lprintk_cpuid_arg
@@ -72,19 +72,23 @@ typedef enum
 
 #if MOS_CONFIG(MOS_PRINTK_WITH_FILENAME)
 #define _lprintk_filename_fmt "%-20s | "
-#define _lprintk_filename_arg , MOS_FILE_LOCATION
+#define _lprintk_filename_arg MOS_FILE_LOCATION
 #else
 #define _lprintk_filename_fmt ""
 #define _lprintk_filename_arg
 #endif
 
 #if MOS_CONFIG(MOS_PRINTK_WITH_TIMESTAMP) || MOS_CONFIG(MOS_PRINTK_WITH_DATETIME) || MOS_CONFIG(MOS_PRINTK_WITH_CPU_ID) || MOS_CONFIG(MOS_PRINTK_WITH_FILENAME)
-#define _lprintk_prefix_fmt              _lprintk_timestamp_fmt _lprintk_datetime_fmt _lprintk_cpuid_fmt _lprintk_filename_fmt
-#define _lprintk_prefix_arg              _lprintk_timestamp_arg _lprintk_datetime_arg _lprintk_cpuid_arg _lprintk_filename_arg
-#define lprintk_wrapper(level, fmt, ...) lprintk(level, "\r\n" _lprintk_prefix_fmt fmt, _lprintk_prefix_arg, ##__VA_ARGS__)
+#define __add_comma(...)    __VA_OPT__(, (__VA_ARGS__))
+#define _lprintk_prefix_fmt _lprintk_timestamp_fmt _lprintk_datetime_fmt _lprintk_cpuid_fmt _lprintk_filename_fmt
+#define _lprintk_prefix_arg __add_comma(_lprintk_timestamp_arg) __add_comma(_lprintk_datetime_arg) __add_comma(_lprintk_cpuid_arg) __add_comma(_lprintk_filename_arg)
 #else
-#define lprintk_wrapper(level, fmt, ...) lprintk(level, "\r\n" fmt, ##__VA_ARGS__)
+#define _lprintk_prefix_fmt ""
+#define _lprintk_prefix_arg // empty so that the comma after fmt is not included
 #endif
+
+// ! comma after fmt is included in '_lprintk_prefix_arg', careful when changing this macro
+#define lprintk_wrapper(level, fmt, ...) lprintk(level, "\r\n" _lprintk_prefix_fmt fmt _lprintk_prefix_arg, ##__VA_ARGS__)
 
 #ifndef pr_fmt
 #define pr_fmt(fmt) fmt // default format string
