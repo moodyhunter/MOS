25592bba073ea535739632e305b1fbed7311b4f2
Moody Liu <mooodyhunter@outlook.com>
2023-10-31 21:06:24 +0000

    idle: create idle tasks for each CPU

--- kernel/tasks/idle_task.c
+++ kernel/tasks/idle_task.c
@@ -16,7 +16,14 @@ static void idle_task(void *arg)
 static void create_idle_task()
 {
     pr_dinfo2(process, "creating the idle task...");
-    kthread_create(idle_task, NULL, "idle");
+
+    for (u32 i = 0; i < platform_info->num_cpus; i++)
+    {
+        pr_dinfo(process, "creating the idle task for CPU %u", i);
+        thread_t *t = kthread_create(idle_task, NULL, "idle");
+        // thread_set_cpu(t, i);
+        MOS_UNUSED(t);
+    }
 }
 
 MOS_INIT(KTHREAD, create_idle_task);
