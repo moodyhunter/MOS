224e53791a18a7d634104bf7545799f9bb5d14a5
Moody Liu <mooodyhunter@outlook.com>
2023-12-20 19:42:16 +0000

    cmake: add PRIVATE_DEFINES for add_mos_library

--- cmake/add_mos_library.cmake
+++ cmake/add_mos_library.cmake
@@ -9,10 +9,10 @@ macro(add_mos_library_do_setup LIBNAME DEFINES PRIVATE_INCLUDE PUBLIC_INCLUDE)
     target_include_directories(${LIBNAME} SYSTEM INTERFACE ${PUBLIC_INCLUDE})
 endmacro()
 
-macro(add_mos_library)
+function(add_mos_library)
     set(options USERSPACE_ONLY HOSTED_ONLY)
     set(oneValueArgs NAME)
-    set(multiValueArgs SOURCES PUBLIC_INCLUDE_DIRECTORIES PRIVATE_INCLUDE_DIRECTORIES LINK_LIBRARIES DEFINES)
+    set(multiValueArgs SOURCES PUBLIC_INCLUDE_DIRECTORIES PRIVATE_INCLUDE_DIRECTORIES LINK_LIBRARIES DEFINES PRIVATE_DEFINES)
     cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     if (NOT ARG_NAME)
@@ -32,6 +32,7 @@ macro(add_mos_library)
         target_link_libraries(${KERNEL_LIB_NAME} PRIVATE gcc mos::include mos::private_include)
         target_link_libraries(mos_kernel PUBLIC ${KERNEL_LIB_NAME})
         target_compile_definitions(${KERNEL_LIB_NAME} PUBLIC ${ARG_DEFINES})
+        target_compile_definitions(${KERNEL_LIB_NAME} PRIVATE ${ARG_PRIVATE_DEFINES})
 
         if ("${ARG_NAME}" STREQUAL "stdlib")
             target_link_libraries(${KERNEL_LIB_NAME} PUBLIC mos::kernel_compile_options)
@@ -45,18 +46,18 @@ macro(add_mos_library)
     endif()
 
     # Create a userspace library
-
     if (NOT ARG_HOSTED_ONLY)
+        # userspace library with minimal libc
         add_library(${ARG_NAME} STATIC ${ARG_SOURCES})
         add_library(mos::${ARG_NAME} ALIAS ${ARG_NAME})
         target_compile_definitions(${ARG_NAME} PUBLIC ${ARG_DEFINES})
+        target_compile_definitions(${ARG_NAME} PRIVATE ${ARG_PRIVATE_DEFINES})
         add_mos_library_do_setup(${ARG_NAME} "" "${ARG_PRIVATE_INCLUDE_DIRECTORIES}" "${ARG_PUBLIC_INCLUDE_DIRECTORIES}")
     endif()
 
     if ("${ARG_NAME}" STREQUAL "stdlib")
         # only need to add these to stdlib
         target_link_libraries(${ARG_NAME} PUBLIC gcc mos::include)
-        # TODO: Remove this once we have a proper userspace libc
         target_compile_options(${ARG_NAME} PUBLIC "-ffreestanding")
         target_compile_definitions(${ARG_NAME} PUBLIC "__MLIBC_ABI_ONLY")
         target_compile_definitions(${ARG_NAME} PUBLIC "__MOS_MINIMAL_LIBC__")
@@ -64,22 +65,25 @@ macro(add_mos_library)
         if (ARG_LINK_LIBRARIES)
             message(FATAL_ERROR "stdlib must not link to other libraries")
         endif()
-    else()
-        if (NOT ARG_HOSTED_ONLY)
-            target_link_libraries(${ARG_NAME} PUBLIC mos::stdlib)
-            target_link_libraries(${ARG_NAME} PUBLIC ${ARG_LINK_LIBRARIES})
-        endif()
-
-        # Create a hosted userspace library
-        add_library(${ARG_NAME}_hosted SHARED ${ARG_SOURCES})
-        add_library(mos::${ARG_NAME}_hosted ALIAS ${ARG_NAME}_hosted)
-
-        add_mos_library_do_setup(${ARG_NAME}_hosted "" "${ARG_PRIVATE_INCLUDE_DIRECTORIES}" "${ARG_PUBLIC_INCLUDE_DIRECTORIES}")
-        target_link_libraries(${ARG_NAME}_hosted PUBLIC mos::include)
-        target_compile_definitions(${ARG_NAME}_hosted PUBLIC ${ARG_DEFINES})
-
-        foreach(lib ${ARG_LINK_LIBRARIES})
-            target_link_libraries(${ARG_NAME}_hosted PUBLIC ${lib}_hosted)
-        endforeach()
+        return()
     endif()
-endmacro()
+
+    if (NOT ARG_HOSTED_ONLY)
+        # library with full libc
+        target_link_libraries(${ARG_NAME} PUBLIC mos::stdlib)
+        target_link_libraries(${ARG_NAME} PUBLIC ${ARG_LINK_LIBRARIES})
+    endif()
+
+    # Create a hosted userspace library
+    add_library(${ARG_NAME}_hosted SHARED ${ARG_SOURCES})
+    add_library(mos::${ARG_NAME}_hosted ALIAS ${ARG_NAME}_hosted)
+
+    add_mos_library_do_setup(${ARG_NAME}_hosted "" "${ARG_PRIVATE_INCLUDE_DIRECTORIES}" "${ARG_PUBLIC_INCLUDE_DIRECTORIES}")
+    target_compile_definitions(${ARG_NAME}_hosted PUBLIC ${ARG_DEFINES})
+    target_compile_definitions(${ARG_NAME}_hosted PRIVATE ${ARG_PRIVATE_DEFINES})
+
+    target_link_libraries(${ARG_NAME}_hosted PUBLIC mos::include)
+    foreach(lib ${ARG_LINK_LIBRARIES})
+        target_link_libraries(${ARG_NAME}_hosted PUBLIC ${lib}_hosted)
+    endforeach()
+endfunction()
