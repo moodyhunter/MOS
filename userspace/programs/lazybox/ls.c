9550bb55f7d136108bcf298953a91b0fef649e28
Moody Liu <mooodyhunter@outlook.com>
2023-12-24 22:39:30 +0000

    lazybox/ls: adapt kernel api change

--- userspace/programs/lazybox/ls.c
+++ userspace/programs/lazybox/ls.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include <dirent.h>
 #include <fcntl.h>
 #include <mos/filesystem/fs_types.h>
 #include <mos/syscall/usermode.h>
@@ -43,24 +44,25 @@ int main(int argc, char *argv[])
         if (sz == 0)
             break;
 
-#define dirent_next(d) ((dir_entry_t *) ((char *) d + d->next_offset))
-        for (const dir_entry_t *dirent = (dir_entry_t *) buffer; (char *) dirent < buffer + sz; dirent = dirent_next(dirent))
+#define dirent_next(d) ((struct dirent *) ((char *) d + d->d_reclen))
+        for (const struct dirent *dirent = (struct dirent *) buffer; (char *) dirent < buffer + sz; dirent = dirent_next(dirent))
         {
             file_stat_t statbuf = { 0 };
-            if (!lstatat(dirfd, dirent->name, &statbuf))
+            if (!lstatat(dirfd, dirent->d_name, &statbuf))
             {
-                fprintf(stderr, "failed to stat '%s'\n", dirent->name);
+                fprintf(stderr, "failed to stat '%s'\n", dirent->d_name);
                 continue;
             }
 
             char perm[10] = "---------";
             file_format_perm(statbuf.perm, perm);
-            printf("%-10llu %-15s %-5u %-5u %-8zu %-10s %-10s", dirent->ino, perm, statbuf.uid, statbuf.gid, statbuf.size, type_to_string[dirent->type], dirent->name);
+            printf("%-10lu %-15s %-5u %-5u %-8zu %-10s %-10s", dirent->d_ino, perm, statbuf.uid, statbuf.gid, statbuf.size, type_to_string[dirent->d_type],
+                   dirent->d_name);
 
-            if (dirent->type == FILE_TYPE_SYMLINK)
+            if (dirent->d_type == FILE_TYPE_SYMLINK)
             {
                 char link[BUFSIZE] = { 0 };
-                size_t sz = syscall_vfs_readlinkat(dirfd, dirent->name, link, BUFSIZE);
+                size_t sz = syscall_vfs_readlinkat(dirfd, dirent->d_name, link, BUFSIZE);
                 bool can_stat = sz > 0 && lstatat(dirfd, link, &statbuf);
                 if (can_stat)
                 {
