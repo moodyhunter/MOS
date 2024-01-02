d4cf0dccea8920749f0ae6f8b748bfdbf2dec4a9
Moody Liu <mooodyhunter@outlook.com>
2024-01-02 12:30:37 +0000

    cmake: fix cross-compiler search path

--- cmake/mos_target_setup.cmake
+++ cmake/mos_target_setup.cmake
@@ -5,6 +5,7 @@ if (NOT DEFINED MOS_ARCH)
 endif()
 
 include(${CMAKE_SOURCE_DIR}/cmake/toolchains/${MOS_ARCH}-mos.cmake)
+set(CMAKE_PREFIX_PATH) # we are only cross-compiling
 
 set(_ARCH_CONFIGURATION_FILE ${CMAKE_SOURCE_DIR}/arch/${MOS_ARCH}/platform_config.cmake)
 set(_ARCH_LISTS ${CMAKE_SOURCE_DIR}/arch/${MOS_ARCH}/CMakeLists.txt)
