9d7c61979529cba318de3d8a4f3b88ea0e158ab2
Moody Liu <mooodyhunter@outlook.com>
2024-01-12 20:11:44 +0000

    virtiod/gpu: use sleep instead of a stupid nop loop

--- userspace/drivers/virtiod/src/drivers/gpu.rs
+++ userspace/drivers/virtiod/src/drivers/gpu.rs
@@ -1,3 +1,5 @@
+use std::thread::sleep;
+
 use virtio_drivers::{device::gpu::VirtIOGpu, transport::Transport};
 
 use crate::hal::MOSHal;
@@ -8,7 +10,9 @@ pub fn start_gpu_driver<T: Transport>(transport: T) {
     let width = width as usize;
     let height = height as usize;
     println!("GPU resolution is {}x{}", width, height);
+
     let fb = gpu.setup_framebuffer().expect("failed to get fb");
+
     for y in 0..height {
         for x in 0..width {
             let idx = (y * width + x) * 4;
@@ -17,16 +21,10 @@ pub fn start_gpu_driver<T: Transport>(transport: T) {
             fb[idx + 2] = (x + y) as u8;
         }
     }
-    gpu.flush().expect("failed to flush");
-    //delay some time
-    println!("virtio-gpu show graphics....");
-    for _ in 0..10000 {
-        for _ in 0..1000 {
-            unsafe {
-                core::arch::asm!("nop");
-            }
-        }
-    }
 
+    gpu.flush().expect("failed to flush");
+
+    println!("virtio-gpu show graphics...");
+    sleep(std::time::Duration::from_secs(10));
     println!("virtio-gpu test finished");
 }
