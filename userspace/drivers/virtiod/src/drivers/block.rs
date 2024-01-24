11cbec87a12a08f658c48658db3320083aff61eb
Moody Liu <mooodyhunter@outlook.com>
2024-01-24 23:49:09 +0000

    virtiod: add VirtIO block device driver

--- userspace/drivers/virtiod/src/drivers/block.rs
+++ userspace/drivers/virtiod/src/drivers/block.rs
@@ -1,10 +1,10 @@
 use std::{
-    cell::OnceCell,
     io::Error,
     sync::{Arc, Mutex},
 };
 
-use librpc_rs::{rpc_server_function, RpcCallContext, RpcServer, RpcStub};
+use librpc_rs::{rpc_server_function, RpcCallContext, RpcCallFuncInfo, RpcServer, RpcStub};
+use protobuf::MessageField;
 use virtio_drivers::{
     device::blk::VirtIOBlk,
     transport::pci::{bus::DeviceFunction, PciTransport},
@@ -12,90 +12,109 @@ use virtio_drivers::{
 
 use crate::{
     hal::MOSHal,
-    mos_rpc::blockdev::{Read_request, Register_dev_request, Register_dev_response, Write_request},
+    mos_rpc::blockdev::{
+        Read_request, Read_response, Register_dev_request, Register_dev_response, Write_request,
+        Write_response,
+    },
+    result_ok,
 };
+
+struct SafeVirtIOBlk(VirtIOBlk<MOSHal, PciTransport>);
+unsafe impl Send for SafeVirtIOBlk {}
 
 #[derive(Clone)]
 struct BlockDevDriver {
     blockdev_manager: RpcStub,
     devname: String,
+    server_name: String,
+    blockdev: Arc<Mutex<SafeVirtIOBlk>>,
 }
-
-static mut DEVICE: OnceCell<Arc<Mutex<VirtIOBlk<MOSHal, PciTransport>>>> = OnceCell::new();
-
-const VIRTIO_BLOCKDEV_SERVER_NAME: &str = "virtio-blockdev";
 
 impl BlockDevDriver {
     pub fn register(&mut self) -> Result<(), Error> {
-        let dev = unsafe { DEVICE.get() }.unwrap().lock().unwrap();
+        let dev = &self.blockdev.lock().unwrap().0;
 
-        let mut req = Register_dev_request::default();
-        req.block_size = 512;
-        req.num_blocks = dev.capacity() / 512;
-        req.server_name = VIRTIO_BLOCKDEV_SERVER_NAME.to_string();
-        req.blockdev_name = self.devname.to_string();
+        let request = Register_dev_request {
+            block_size: 512,
+            num_blocks: dev.capacity() / 512,
+            server_name: self.server_name.clone(),
+            blockdev_name: self.devname.clone(),
+            ..Default::default()
+        };
 
-        println!("req: {:?}", req);
-        let resp: Register_dev_response = self.blockdev_manager.create_pb_call(1, &req)?;
-        println!("resp: {:?}", resp);
+        let resp: Register_dev_response = self.blockdev_manager.create_pb_call(1, &request)?;
+        println!("registered with blockdev id {}", resp.id);
         Ok(())
     }
 
     pub fn on_read(&mut self, ctx: &mut RpcCallContext) -> Result<(), Error> {
-        let arg: Read_request = ctx.get_arg_pb(0)?;
-        println!("arg: {:?}", arg);
+        let dev = &mut self.blockdev.lock().unwrap().0;
 
-        Ok(())
+        let arg: Read_request = ctx.get_arg_pb(0)?;
+        println!(
+            "read reqeust: block {}, nblocks {}",
+            arg.n_boffset, arg.n_blocks
+        );
+
+        let mut buf = vec![0u8; 512 * arg.n_blocks as usize];
+
+        dev.read_blocks(arg.n_boffset as _, &mut buf)
+            .expect("failed to read");
+
+        let resp = Read_response {
+            result: result_ok!(),
+            data: buf,
+            ..Default::default()
+        };
+
+        ctx.write_response_pb(&resp)
     }
 
     pub fn on_write(&mut self, ctx: &mut RpcCallContext) -> Result<(), Error> {
         let arg: Write_request = ctx.get_arg_pb(0)?;
-        println!("arg: {:?}", arg);
-        Ok(())
+
+        println!(
+            "write reqeust: block {}, nblocks {}",
+            arg.n_boffset, arg.n_blocks
+        );
+
+        // let mut dev = unsafe { BLOCKDEV.get() }.unwrap().lock().unwrap();
+        let dev = &mut self.blockdev.lock().unwrap().0;
+
+        dev.write_blocks(arg.n_boffset as _, &arg.data)
+            .expect("failed to write");
+
+        let resp = Write_response {
+            result: result_ok!(),
+            ..Default::default()
+        };
+
+        ctx.write_response_pb(&resp)
     }
-
-    // let mut input = [0xffu8; 512];
-    // let mut output = [0; 512];
-    // for i in 0..32 {
-    //     let mut j = 0;
-    //     for x in input.iter_mut() {
-    //         *x = j as u8;
-    //         j += 1;
-    //     }
-
-    //     blk.write_blocks(i, &input).expect("failed to write");
-    //     blk.read_blocks(i, &mut output).expect("failed to read");
-    //     assert_eq!(input, output);
-    //     println!("read/write block {} passed", i);
-    // }
 }
+
+const FUNCTIONS: &[RpcCallFuncInfo<BlockDevDriver>] = &[
+    rpc_server_function!(1, BlockDevDriver::on_read, Buffer),
+    rpc_server_function!(2, BlockDevDriver::on_write, Buffer),
+];
 
 // virtiod --vendor-id 1af4 --device-id 1001 --location 0300 --mmio-base b0000000
 pub fn run_blockdev(transport: PciTransport, function: DeviceFunction) -> Result<(), Error> {
-    unsafe {
-        assert!(DEVICE.get().is_none());
-        match DEVICE.set(Arc::new(Mutex::new(
-            VirtIOBlk::new(transport).expect("failed to create blockdev"),
-        ))) {
-            Ok(_) => {}
-            Err(_) => panic!("failed to set DEVICE"),
-        }
-    };
+    let devname = format!(
+        "{:02x}:{:02x}:{:02x}",
+        function.bus, function.device, function.function
+    );
 
     let mut driver = BlockDevDriver {
         blockdev_manager: RpcStub::new("mos.blockdev-manager")?,
-        devname: format!(
-            "virtio-blockdev-{:02x}:{:02x}:{:02x}",
-            function.bus, function.device, function.function
-        ),
+        devname: format!("virtio-blockdev:{}", devname),
+        server_name: format!("blockdev.virtio.{}", devname),
+        blockdev: Arc::new(Mutex::new(SafeVirtIOBlk(
+            VirtIOBlk::new(transport).expect("failed to create blockdev"),
+        ))),
     };
 
-    let functions = &[
-        rpc_server_function!(0, BlockDevDriver::on_read, Buffer),
-        rpc_server_function!(1, BlockDevDriver::on_write, Buffer),
-    ];
-
-    let mut rpc_server = RpcServer::create(VIRTIO_BLOCKDEV_SERVER_NAME, functions)?;
+    let mut rpc_server = RpcServer::create(&driver.server_name, FUNCTIONS)?;
 
     driver.register()?;
 
--- userspace/drivers/virtiod/src/mos_rpc/mod.rs
+++ userspace/drivers/virtiod/src/mos_rpc/mod.rs
@@ -1,1 +1,22 @@
 include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
+
+#[macro_export]
+macro_rules! result_ok {
+    () => {
+        MessageField::from(Some(crate::mos_rpc::mos_rpc::Result {
+            success: true,
+            ..Default::default()
+        }))
+    };
+}
+
+#[macro_export]
+macro_rules! result_err {
+    ($msg:expr) => {
+        MessageField::from(Some(crate::mos_rpc::mos_rpc::Result {
+            success: false,
+            error: Some($msg.to_string()),
+            ..Default::default()
+        }))
+    };
+}
