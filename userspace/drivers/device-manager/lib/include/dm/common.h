40892d8c0e620fe39097a3b7d5eda6137757a1ad
Moody Liu <mooodyhunter@outlook.com>
2024-06-02 18:17:15 +0100

    chore: remove unused files & structures

--- userspace/drivers/device-manager/lib/include/dm/common.h
+++ userspace/drivers/device-manager/lib/include/dm/common.h
@@ -12,22 +12,3 @@
 RPC_DEFINE_ENUMS(dm, DM, DM_RPCS_X)
 
 #define MOS_DEVICE_MANAGER_SERVICE_NAME "mos.device-manager"
-
-typedef struct device_t device_t;
-typedef struct device_driver_t device_driver_t;
-
-typedef struct device_driver_t
-{
-    const char *driver_name;
-    const char *author;
-
-    bool (*probe)(device_driver_t *driver, device_t *dev);
-} device_driver_t;
-
-typedef struct device_t
-{
-    const char *name;
-    const char *description;
-    device_type_t type;
-    device_driver_t *driver;
-} device_t;
