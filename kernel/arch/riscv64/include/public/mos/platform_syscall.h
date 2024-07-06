9cb065bd36cca9f992d98ac3d73dbd54b7b5245b
Moody Liu <mooodyhunter@outlook.com>
2024-07-06 20:19:56 +0100

    platform/riscv64: update platform_syscall.h

--- kernel/arch/riscv64/include/public/mos/platform_syscall.h
+++ kernel/arch/riscv64/include/public/mos/platform_syscall.h
@@ -5,51 +5,50 @@
 #include <mos/mos_global.h>
 #include <mos/types.h>
 
+#define do_syscall(n, _a0, _a1, _a2, _a3, _a4, _a5)                                                                                                                      \
+    register long a0 __asm__("a0") = _a0;                                                                                                                                \
+    register long a1 __asm__("a1") = _a1;                                                                                                                                \
+    register long a2 __asm__("a2") = _a2;                                                                                                                                \
+    register long a3 __asm__("a3") = _a3;                                                                                                                                \
+    register long a4 __asm__("a4") = _a4;                                                                                                                                \
+    register long a5 __asm__("a5") = _a5;                                                                                                                                \
+    register long syscall_id __asm__("a7") = n;                                                                                                                          \
+    __asm__ volatile("ecall" : "+r"(a0) : "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(syscall_id));                                                                 \
+    return a0
+
 should_inline reg_t platform_syscall0(reg_t number)
 {
-    reg_t ret;
-    __asm__ volatile("ecall" : "=r"(ret) : "r"(number) : "memory");
-    return ret;
+    do_syscall(number, 0, 0, 0, 0, 0, 0);
 }
 
 should_inline reg_t platform_syscall1(reg_t number, reg_t arg0)
 {
-    reg_t ret;
-    __asm__ volatile("ecall" : "=r"(ret) : "r"(number), "r"(arg0) : "memory");
-    return ret;
+    do_syscall(number, arg0, 0, 0, 0, 0, 0);
 }
 
 should_inline reg_t platform_syscall2(reg_t number, reg_t arg0, reg_t arg1)
 {
-    reg_t ret;
-    __asm__ volatile("ecall" : "=r"(ret) : "r"(number), "r"(arg0), "r"(arg1) : "memory");
-    return ret;
+    do_syscall(number, arg0, arg1, 0, 0, 0, 0);
 }
 
 should_inline reg_t platform_syscall3(reg_t number, reg_t arg0, reg_t arg1, reg_t arg2)
 {
-    reg_t ret;
-    __asm__ volatile("ecall" : "=r"(ret) : "r"(number), "r"(arg0), "r"(arg1), "r"(arg2) : "memory");
-    return ret;
+    do_syscall(number, arg0, arg1, arg2, 0, 0, 0);
 }
 
 should_inline reg_t platform_syscall4(reg_t number, reg_t arg0, reg_t arg1, reg_t arg2, reg_t arg3)
 {
-    reg_t ret;
-    __asm__ volatile("ecall" : "=r"(ret) : "r"(number), "r"(arg0), "r"(arg1), "r"(arg2), "r"(arg3) : "memory");
-    return ret;
+    do_syscall(number, arg0, arg1, arg2, arg3, 0, 0);
 }
 
 should_inline reg_t platform_syscall5(reg_t number, reg_t arg0, reg_t arg1, reg_t arg2, reg_t arg3, reg_t arg4)
 {
-    reg_t ret;
-    __asm__ volatile("ecall" : "=r"(ret) : "r"(number), "r"(arg0), "r"(arg1), "r"(arg2), "r"(arg3), "r"(arg4) : "memory");
-    return ret;
+    do_syscall(number, arg0, arg1, arg2, arg3, arg4, 0);
 }
 
 should_inline reg_t platform_syscall6(reg_t number, reg_t arg0, reg_t arg1, reg_t arg2, reg_t arg3, reg_t arg4, reg_t arg5)
 {
-    reg_t ret;
-    __asm__ volatile("ecall" : "=r"(ret) : "r"(number), "r"(arg0), "r"(arg1), "r"(arg2), "r"(arg3), "r"(arg4), "r"(arg5) : "memory");
-    return ret;
+    do_syscall(number, arg0, arg1, arg2, arg3, arg4, arg5);
 }
+
+#undef do_syscall
