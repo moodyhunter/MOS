04100cffe13fc9502de27f30da5568a99b730d06
Moody Liu <mooodyhunter@outlook.com>
2024-07-12 03:08:25 +0100

    platform/riscv64: update platform_syscall

--- kernel/arch/riscv64/include/public/mos/platform_syscall.h
+++ kernel/arch/riscv64/include/public/mos/platform_syscall.h
@@ -10,50 +10,77 @@ enum
     RISCV64_SYSCALL_SET_TP = 0,
 };
 
-#define do_syscall(n, _a0, _a1, _a2, _a3, _a4, _a5)                                                                                                                      \
-    register long a0 __asm__("a0") = _a0;                                                                                                                                \
-    register long a1 __asm__("a1") = _a1;                                                                                                                                \
-    register long a2 __asm__("a2") = _a2;                                                                                                                                \
-    register long a3 __asm__("a3") = _a3;                                                                                                                                \
-    register long a4 __asm__("a4") = _a4;                                                                                                                                \
-    register long a5 __asm__("a5") = _a5;                                                                                                                                \
-    register long syscall_id __asm__("a7") = n;                                                                                                                          \
-    __asm__ volatile("ecall" : "+r"(a0) : "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(syscall_id));                                                                 \
-    return a0
+#define do_syscall(...) __asm__ volatile("ecall" : "=r"(a0) : __VA_ARGS__ : "memory")
 
-should_inline reg_t platform_syscall0(reg_t number)
+should_inline reg_t platform_syscall0(long n)
 {
-    do_syscall(number, 0, 0, 0, 0, 0, 0);
+    register long a7 __asm__("a7") = n;
+    register long a0 __asm__("a0");
+    do_syscall("r"(a7));
+    return a0;
 }
 
-should_inline reg_t platform_syscall1(reg_t number, reg_t arg0)
+should_inline reg_t platform_syscall1(long n, long a)
 {
-    do_syscall(number, arg0, 0, 0, 0, 0, 0);
+    register long a7 __asm__("a7") = n;
+    register long a0 __asm__("a0") = a;
+    do_syscall("r"(a0), "r"(a7));
+    return a0;
 }
 
-should_inline reg_t platform_syscall2(reg_t number, reg_t arg0, reg_t arg1)
+should_inline reg_t platform_syscall2(long n, long a, long b)
 {
-    do_syscall(number, arg0, arg1, 0, 0, 0, 0);
+    register long a7 __asm__("a7") = n;
+    register long a0 __asm__("a0") = a;
+    register long a1 __asm__("a1") = b;
+    do_syscall("r"(a0), "r"(a7), "r"(a1));
+    return a0;
 }
 
-should_inline reg_t platform_syscall3(reg_t number, reg_t arg0, reg_t arg1, reg_t arg2)
+should_inline reg_t platform_syscall3(long n, long a, long b, long c)
 {
-    do_syscall(number, arg0, arg1, arg2, 0, 0, 0);
+    register long a7 __asm__("a7") = n;
+    register long a0 __asm__("a0") = a;
+    register long a1 __asm__("a1") = b;
+    register long a2 __asm__("a2") = c;
+    do_syscall("r"(a0), "r"(a7), "r"(a1), "r"(a2));
+    return a0;
 }
 
-should_inline reg_t platform_syscall4(reg_t number, reg_t arg0, reg_t arg1, reg_t arg2, reg_t arg3)
+should_inline reg_t platform_syscall4(long n, long a, long b, long c, long d)
 {
-    do_syscall(number, arg0, arg1, arg2, arg3, 0, 0);
+    register long a7 __asm__("a7") = n;
+    register long a0 __asm__("a0") = a;
+    register long a1 __asm__("a1") = b;
+    register long a2 __asm__("a2") = c;
+    register long a3 __asm__("a3") = d;
+    do_syscall("r"(a0), "r"(a7), "r"(a1), "r"(a2), "r"(a3));
+    return a0;
 }
 
-should_inline reg_t platform_syscall5(reg_t number, reg_t arg0, reg_t arg1, reg_t arg2, reg_t arg3, reg_t arg4)
+should_inline reg_t platform_syscall5(long n, long a, long b, long c, long d, long e)
 {
-    do_syscall(number, arg0, arg1, arg2, arg3, arg4, 0);
+    register long a7 __asm__("a7") = n;
+    register long a0 __asm__("a0") = a;
+    register long a1 __asm__("a1") = b;
+    register long a2 __asm__("a2") = c;
+    register long a3 __asm__("a3") = d;
+    register long a4 __asm__("a4") = e;
+    do_syscall("r"(a0), "r"(a7), "r"(a1), "r"(a2), "r"(a3), "r"(a4));
+    return a0;
 }
 
-should_inline reg_t platform_syscall6(reg_t number, reg_t arg0, reg_t arg1, reg_t arg2, reg_t arg3, reg_t arg4, reg_t arg5)
+should_inline reg_t platform_syscall6(long n, long a, long b, long c, long d, long e, long f)
 {
-    do_syscall(number, arg0, arg1, arg2, arg3, arg4, arg5);
+    register long a7 __asm__("a7") = n;
+    register long a0 __asm__("a0") = a;
+    register long a1 __asm__("a1") = b;
+    register long a2 __asm__("a2") = c;
+    register long a3 __asm__("a3") = d;
+    register long a4 __asm__("a4") = e;
+    register long a5 __asm__("a5") = f;
+    do_syscall("r"(a0), "r"(a7), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5));
+    return a0;
 }
 
 #undef do_syscall
