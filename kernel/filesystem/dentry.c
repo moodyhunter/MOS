3f6399ef0af6b1264819d0945d141b8768cd6738
Moody Liu <mooodyhunter@outlook.com>
2024-08-08 00:16:06 +0100

    vfs: fix symlink resolution in dentry_resolve_to_parent

--- kernel/filesystem/dentry.c
+++ kernel/filesystem/dentry.c
@@ -14,6 +14,7 @@
 #include "mos/tasks/process.h"
 #include "mos/tasks/task_types.h"
 
+#include <mos/filesystem/fs_types.h>
 #include <mos/lib/structures/hashmap_common.h>
 #include <mos_stdio.h>
 #include <mos_stdlib.h>
@@ -75,18 +76,18 @@ static dentry_t *dentry_resolve_to_parent(dentry_t *base_dir, dentry_t *root_dir
     {
         pr_dinfo2(dcache, "lookup parent: current segment '%s'", current_seg);
         const char *const next = strtok_r(NULL, PATH_DELIM_STR, &saveptr);
+        if (parent_ref->inode->type == FILE_TYPE_SYMLINK)
+        {
+            // this is the real interesting dir
+            dentry_t *const parent_real_ref = dentry_resolve_follow_symlink(parent_ref, RESOLVE_EXPECT_EXIST | RESOLVE_EXPECT_DIR);
+            dentry_unref(parent_ref);
+            if (unlikely(parent_real_ref == NULL))
+                return ERR_PTR(-ENOENT); // the symlink target does not exist
+            parent_ref = parent_real_ref;
+        }
+
         if (next == NULL)
         {
-            if (parent_ref->inode->type == FILE_TYPE_SYMLINK)
-            {
-                // this is the real interesting dir
-                dentry_t *const parent_real_ref = dentry_resolve_follow_symlink(parent_ref, RESOLVE_EXPECT_EXIST | RESOLVE_EXPECT_DIR);
-                dentry_unref(parent_ref);
-                if (unlikely(parent_real_ref == NULL))
-                    return ERR_PTR(-ENOENT); // the symlink target does not exist
-                parent_ref = parent_real_ref;
-            }
-
             // "current_seg" is the last segment of the path
             if (last_seg_out != NULL)
             {
